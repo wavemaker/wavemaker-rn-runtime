8f591a6cc3eab26c88e1396c36af586c4154665a
Boyina Srinivasa Rao <boyinasrinivasulu@gmail.com>
2021-05-26 10:07:25 +0530

    WMS-20621:added bootstrap styles to anchor.

--- src/components/basic/anchor/anchor.styles.ts
+++ src/components/basic/anchor/anchor.styles.ts
@@ -1,26 +1,57 @@
 import BASE_THEME from '@wavemaker/app-rn-runtime/styles/theme';
 
 export const DEFAULT_CLASS = 'app-anchor';
-const textStyles = {
-    color: 'blue',
-    fontSize: 14
-};
+
 export const DEFAULT_STYLES = {
     root: {
         color: 'blue',
         flexDirection: 'row',
         alignSelf: 'flex-start'
     },
-    text: textStyles,
+    text: {
+        color: 'blue',
+        fontSize: 12,
+        textDecoration: 'underline'
+    },
     badge: {
+        backgroundColor: '#f50057',
+        color: '#ffffff',
         alignSelf: 'flexStart',
         position: 'relative',
         top: -12,
         left: -6
     },
     icon: {
-        text: textStyles
+        text: {
+            color: 'blue',
+            fontSize: 14
+        }
     }
 };
 
 BASE_THEME.addStyle(DEFAULT_CLASS, '', DEFAULT_STYLES);
+
+const getColordLinkStyles = (color: string) => {
+    return {
+        root: {
+            color: color
+        },
+        text: {
+            color: color
+        },
+        icon: {
+            text: {
+                color: color
+            }
+        }
+    };
+};
+
+BASE_THEME.addStyle('link-primary', DEFAULT_CLASS, getColordLinkStyles('#0d6efd'));
+BASE_THEME.addStyle('link-secondary', DEFAULT_CLASS, getColordLinkStyles('#565e64'));
+BASE_THEME.addStyle('link-success', DEFAULT_CLASS, getColordLinkStyles('#198754'));
+BASE_THEME.addStyle('link-danger', DEFAULT_CLASS, getColordLinkStyles('#b02a37'));
+BASE_THEME.addStyle('link-warning', DEFAULT_CLASS, getColordLinkStyles('#ffc107'));
+BASE_THEME.addStyle('link-info', DEFAULT_CLASS, getColordLinkStyles('#0dcaf0'));
+BASE_THEME.addStyle('link-light', DEFAULT_CLASS, getColordLinkStyles('#f8f9fa'));
+BASE_THEME.addStyle('link-dark', DEFAULT_CLASS, getColordLinkStyles('#212529'));
