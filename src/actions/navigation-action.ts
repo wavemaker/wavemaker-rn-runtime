0660e608af8b829c9468d1d318167b60108752d7
Nikhilesh K V <nikhilesh.kashibhatla@wavemaker.com>
2021-06-07 15:32:17 +0530

    WMS-20632: moved necessary logic to nav action

--- src/actions/navigation-action.ts
+++ src/actions/navigation-action.ts
@@ -1,5 +1,6 @@
 import AppConfig from "../core/AppConfig";
 import { ActionConfig, BaseAction } from "./base-action";
+import {merge} from "lodash";
 
 export interface NavigationActionConfig extends ActionConfig {
     appConfig: AppConfig;
@@ -7,13 +8,14 @@ export interface NavigationActionConfig extends ActionConfig {
 }
 
 export class NavigationAction extends BaseAction {
-    category: string = 'wm.NavigationVariable';
     constructor(config: NavigationActionConfig) {
         super(config);
     }
 
     public invoke(params?: {}, onSuccess?: Function, onError?: Function): Promise<NavigationAction> {
         const config = this.config as NavigationActionConfig;
+        // @ts-ignore
+        params = params?.data ? merge(this.config.paramProvider(), params.data) : merge(this.config.paramProvider(), this.dataSet);
         return super.invoke(params, onSuccess, onError).then(() => {
             config.operation === 'goToPreviousPage' ? config.appConfig.currentPage?.goBack() : config.appConfig.currentPage?.goToPage(this.params.pageName, this.params);
         }).then(() => {
