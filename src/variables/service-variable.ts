f0230c5ea63e1d173f4ea8bb4ea3d7d6dfde9f49
Nikhilesh K V <nikhilesh.kashibhatla@wavemaker.com>
2021-06-10 22:24:26 +0530

    WMS-20706: service variable changes

--- src/variables/service-variable.ts
+++ src/variables/service-variable.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
 import { BaseVariable, VariableConfig } from "./base-variable";
-import { isObject, isNumber, forEach, get } from "lodash";
+import { isObject, isNumber, forEach, get, isArray, toLower, merge, includes, toUpper } from "lodash";
+import AppConfig from "@wavemaker/app-rn-runtime/core/AppConfig";
+import { WS_CONSTANTS } from "./utils/variable.constants";
 
 export interface ServiceVariableConfig extends VariableConfig {
     serviceInfo: any;
+    appConfig: AppConfig;
+    onCanUpdate: any,
+    onBeforeUpdate: any,
+    onResult: any,
+    onBeforeDatasetReady: any
 }
 
 export class ServiceVariable extends BaseVariable {
@@ -19,27 +26,107 @@ export class ServiceVariable extends BaseVariable {
         if (!params) {
           params = Object.keys(this.params).length ? this.params : undefined;
         }
-        super.invoke(params);
         const config = (this.config as ServiceVariableConfig);
-        const queryParams = config.serviceInfo.parameters
-            .filter((p:any) => p.parameterType === 'query')
-            .map((p: any) => {
-                return `${p.name}=${this.params[p.name]}`
-            }).join('&');
+        const onBeforeCallback = config.onBeforeUpdate && config.onBeforeUpdate(this, params);
+        if (onBeforeCallback) {
+          params = onBeforeCallback;
+        }
+        super.invoke(params);
+        const proxyURL = config.appConfig?.url ? config.appConfig?.url + '/services' : '';
+        let queryParams = '',
+          headers: any = {},
+          requestBody,
+          url = get(config.serviceInfo, 'proxySettings.mobile') === true ? proxyURL + config.serviceInfo.relativePath : config.serviceInfo.directPath,
+          requiredParamMissing: any = [],
+          pathParamRex;
+
+        forEach(config.serviceInfo.parameters, (param) => {
+          let paramValue = this.params[param.name];
+          if (paramValue) {
+            switch (param.parameterType.toUpperCase()) {
+              case 'QUERY':
+                if (!queryParams) {
+                  queryParams = '?' + param.name + '=' + encodeURIComponent(paramValue);
+                } else {
+                  queryParams += '&' + param.name + '=' + encodeURIComponent(paramValue);
+                }
+                break;
+              case 'PATH':
+                /* replacing the path param based on the regular expression in the relative path */
+                pathParamRex = new RegExp('\\s*\\{\\s*' + param.name + '(:\\.\\+)?\\s*\\}\\s*');
+                url = url.replace(pathParamRex, paramValue);
+                break;
+              case 'HEADER':
+                // @ts-ignore
+                headers[param.name] = paramValue;
+                break;
+              case 'BODY':
+                  requestBody = paramValue;
+                break;
+              case 'FORMDATA':
+                break;
+            }
+          } else if (param.required) {
+            requiredParamMissing.push(param.name || param.id);
+          }
+        });
+
+        if (requiredParamMissing.length) {
+          console.error({
+            'error': {
+              'type': 'Required field(s) missing',
+              'field': requiredParamMissing.join(',')
+            }
+          });
+          return;
+        }
+
+        // Setting appropriate content-Type for request accepting request body like POST, PUT, etc
+        if (!includes(WS_CONSTANTS.NON_BODY_HTTP_METHODS, toUpper(config.serviceInfo.methodType))) {
+          /*Based on the formData browser will automatically set the content type to 'multipart/form-data' and webkit boundary*/
+          if (!(config.serviceInfo.methodType.consumes && (config.serviceInfo.methodType.consumes[0] === WS_CONSTANTS.CONTENT_TYPES.MULTIPART_FORMDATA))) {
+            headers['Content-Type'] = (config.serviceInfo.methodType.consumes && config.serviceInfo.methodType.consumes[0]) || 'application/json';
+          }
+        }
+
+        // if the consumes has application/x-www-form-urlencoded and
+        // if the http request of given method type can have body send the queryParams as Form Data
+        if (includes(config.serviceInfo.methodType.consumes, WS_CONSTANTS.CONTENT_TYPES.FORM_URL_ENCODED)
+          && !includes(WS_CONSTANTS.NON_BODY_HTTP_METHODS, (config.serviceInfo.methodType || '').toUpperCase())) {
+          // remove the '?' at the start of the queryParams
+          if (queryParams) {
+            requestBody = (requestBody ? requestBody + '&' : '') + queryParams.substring(1);
+          }
+          headers['Content-Type'] = WS_CONSTANTS.CONTENT_TYPES.FORM_URL_ENCODED;
+        } else {
+          url += queryParams;
+        }
+
         this.params = this.config.paramProvider();
-        return axios.get(config.serviceInfo.directPath + '?'+ queryParams).then(result => {
+        // @ts-ignore
+        return axios[config.serviceInfo.methodType](url, requestBody, {headers: headers}).then(result => {
+            config.onResult && config.onResult(this, result.data, result);
             this.dataSet = result.data;
+            // if a primitive type response is returned, wrap it in an object
+            this.dataSet = (!isObject(result.data)) ? {'value': result.data} : result.data;
+
+            // EVENT: ON_PREPARE_SETDATA
+            const newDataSet = config.onBeforeDatasetReady && config.onBeforeDatasetReady(this, this.dataSet);
+            if (newDataSet) {
+              // setting returned value to the dataSet
+              this.dataSet = newDataSet;
+            }
         }).then(() => {
             config.onSuccess && config.onSuccess(this, this.dataSet);
             return this;
-        }, () => {
-            config.onError && config.onError(this, null);
+        }, (error: any) => {
+            config.onError && config.onError(this, null, error);
             return this;
         });
     }
 
     setInput(key: any, val?: any, options?: any) {
-      this.params = this._setInput(this.params, key, val, options);
+      this.params = merge(this.config.paramProvider(), this._setInput(this.params, key, val, options));
       return this.params;
     }
 
---
+++ src/variables/utils/variable.constants.ts
@@ -0,0 +1,8 @@
+export const WS_CONSTANTS = {
+  NON_BODY_HTTP_METHODS: ['GET', 'HEAD'],
+  CONTENT_TYPES: {
+    FORM_URL_ENCODED: 'application/x-www-form-urlencoded',
+    MULTIPART_FORMDATA: 'multipart/form-data',
+    OCTET_STREAM: 'application/octet-stream'
+  }
+};
