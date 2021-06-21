8a2d225a35c5ea1c46c46afc7fef1f4e9658254e
Nikhilesh K V <nikhilesh.kashibhatla@wavemaker.com>
2021-06-21 15:30:22 +0530

    WMS-20703: toggle widget

---
+++ src/components/input/toggle/toggle.props.ts
@@ -0,0 +1,8 @@
+import { BaseProps } from '@wavemaker/app-rn-runtime/core/base.component';
+
+export default class WmToggleProps extends BaseProps {
+  checkedvalue: any;
+  uncheckedvalue: any;
+  datavalue: any;
+  readonly? = false;
+}
