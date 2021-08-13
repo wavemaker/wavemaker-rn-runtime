635a91778178f43481bc5a50510c66fe7fd75df2
swathi <swathi.anupuram@wavemaker.com>
2021-08-13 16:20:51 +0530

    WMS-20886: Added support to 'calendar' service in device variables

---
+++ src/runtime/services/device/contacts-service.ts
@@ -0,0 +1,38 @@
+import * as Contacts from 'expo-contacts';
+import permissionManager from '@wavemaker/app-rn-runtime/runtime/services/device/permissions';
+import { ContactsInput } from '@wavemaker/app-rn-runtime/core/device/contacts-service';
+import { ContactsOutput, PhoneNumber} from "@wavemaker/app-rn-runtime/variables/device/contacts/get-contacts.operation";
+
+
+export class ContactsService {
+
+  constructor() {}
+
+  public getContacts(params: ContactsInput): Promise<ContactsOutput> {
+    return new Promise((resolve, reject) => {
+      permissionManager.requestPermissions('contacts').then(() => {
+
+        return Contacts.getContactsAsync({
+          fields: [ Contacts.Fields.ID, Contacts.Fields.Name, Contacts.Fields.PhoneNumbers],
+          name: params.contactFilter
+        }).then((value: any) => {
+          let contacts: any = [];
+          value.data.forEach((c: any) => {
+            const numbers = [] as Array<PhoneNumber>;
+            c.phoneNumbers?.forEach((num: any) => {
+              numbers.push({
+                value: num.number as string
+              });
+            });
+            contacts.push({
+              id: c.id,
+              displayName: c.name,
+              phoneNumbers: numbers
+            });
+          });
+          return resolve(contacts);
+        });
+      }, reject);
+    });
+  }
+}
--- src/runtime/services/device/permissions.ts
+++ src/runtime/services/device/permissions.ts
@@ -1,5 +1,6 @@
 import * as Permissions from "expo-permissions";
 import * as Location from "expo-location";
+import * as Contacts from 'expo-contacts';
 import * as Calendar from 'expo-calendar';
 
 const rejectionMsgMap = new Map<string, string>();
@@ -7,6 +8,8 @@ const rejectionMsgMap = new Map<string, string>();
 rejectionMsgMap.set('camera', 'camera permission is required to capture image');
 rejectionMsgMap.set('video', 'camera and audio permissions are required to capture video');
 rejectionMsgMap.set('location', 'enable geolocation permission to access the location');
+rejectionMsgMap.set('contacts', 'enable contacts permission to access the contacts');
+rejectionMsgMap.set('calendar', 'enable calendar permission to access the calendar events');
 
 export default {
   requestPermissions: (type: string) => {
@@ -18,6 +21,8 @@ export default {
       query = Permissions.askAsync(Permissions.CAMERA);
     } else if (type === 'video') {
       query = Permissions.askAsync(Permissions.CAMERA, Permissions.AUDIO_RECORDING);
+    } else if(type === 'contacts') {
+      query = Contacts.requestPermissionsAsync();
     } else if(type === 'calendar') {
       query = Calendar.requestCalendarPermissionsAsync();
     }
