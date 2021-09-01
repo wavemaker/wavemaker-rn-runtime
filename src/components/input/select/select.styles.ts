0a056d697bc4e460df36d877bd20d565b86ba8ab
Boyina Srinivasa Rao <srinivasa.boyina@wavemaker.com>
2021-09-01 16:40:02 +0530

    WMS-20958: modified select styles.

--- src/components/input/select/select.styles.ts
+++ src/components/input/select/select.styles.ts
@@ -1,11 +1,23 @@
 import BASE_THEME from '@wavemaker/app-rn-runtime/styles/theme';
 import { BaseStyles } from '@wavemaker/app-rn-runtime/core/base.component';
+import ThemeVariables from '@wavemaker/app-rn-runtime/styles/theme.variables';
 
 export type WmSelectStyles = BaseStyles & {};
 
 export const DEFAULT_CLASS = 'app-select';
 export const DEFAULT_STYLES: WmSelectStyles = {
-    root: {},
+    root: {
+      height: 38,
+      paddingTop: 8,
+      paddingBottom: 8,
+      paddingLeft: 12,
+      paddingRight: 12,
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: ThemeVariables.inputBorderColor,
+      backgroundColor: ThemeVariables.inputBackgroundColor,
+      borderRadius: 4
+    },
     text: {}
 };
 
