a33a3e6f2193284f5c2462e54408c5cbd701eedf
Nikhilesh K V <nikhilesh.kashibhatla@wavemaker.com>
2021-09-23 18:37:19 +0530

    WMS-21159: form with service var

--- src/components/data/form/form.component.tsx
+++ src/components/data/form/form.component.tsx
@@ -11,9 +11,6 @@ import {VARIABLE_LOGGER} from "@wavemaker/app-rn-runtime/variables/base-variable
 import WmLabel from "@wavemaker/app-rn-runtime/components/basic/label/label.component";
 import WmIcon from "@wavemaker/app-rn-runtime/components/basic/icon/icon.component";
 
-import injector from '@wavemaker/app-rn-runtime/core/injector';
-import AppConfig from '@wavemaker/app-rn-runtime/core/AppConfig';
-
 export class WmFormState extends BaseComponentState<WmFormProps> {
   isValid = false;
 }
@@ -85,7 +82,7 @@ export default class WmForm extends BaseComponent<WmFormProps, WmFormState, WmFo
       this.invokeEventCallback('onSubmit', [ null, this.proxy, formData ]);
     }
   }
-  
+
   onResultCb(response: any, status: string, event?: any) {
     this.invokeEventCallback('onResult', [ null, this.proxy, response ]);
     if (status) {
