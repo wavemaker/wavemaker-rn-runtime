3ce39e19f17593f645ff6124bec3dd8433b02723
swathi <swathi.anupuram@wavemaker.com>
2021-10-21 17:49:34 +0530

    Date/Datetime[WMS-21288]: Selected date and time is not displaying on the widget if it is loaded with selected locale.

--- src/runtime/services/app-i18n.service.tsx
+++ src/runtime/services/app-i18n.service.tsx
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import AppConfig from '@wavemaker/app-rn-runtime/core/AppConfig';
 import injector from '@wavemaker/app-rn-runtime/core/injector';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { isWebPreviewMode } from '@wavemaker/app-rn-runtime/core/utils';
 
 
 const APP_LOCALE_ROOT_PATH = '/resources/i18n';
@@ -14,37 +15,46 @@ class AppI18nService {
     dateTimeFormat: string = '';
     currencyCode: string = '';
 
-    constructor() {
-        this.getSelectedLocale().then((locale) => this.selectedLocale = locale || '');
-    }
+    constructor() {}
 
     loadAppLocaleBundle(url: string) {
         const appConfig = injector.get<AppConfig>('APP_CONFIG');
-        if (!this.selectedLocale) {
+        return this.getSelectedLocale().then((locale) => {
+          appConfig.selectedLocale = this.selectedLocale = locale || '';
+          if (!this.selectedLocale) {
             this.selectedLocale = appConfig.appProperties.preferBrowserLang == 'false'? appConfig.appProperties.defaultLanguage : this.defaultSupportedLocale;
             appConfig.selectedLocale = this.selectedLocale;
-        }
-        const path = `${url + APP_LOCALE_ROOT_PATH}/${this.selectedLocale}.json`;
-        return axios.get(path)
+          }
+          const path = `${url + APP_LOCALE_ROOT_PATH}/${this.selectedLocale}.json`;
+          return axios.get(path)
             .then((bundle) => {
-                this.dateFormat = bundle.data.formats.date;
-                this.timeFormat = bundle.data.formats.time;
-                this.currencyCode = bundle.data.formats.currency;
-                this.dateTimeFormat = this.dateFormat + ' ' + this.timeFormat;
-                return bundle;
+              this.dateFormat = bundle.data.formats.date;
+              this.timeFormat = bundle.data.formats.time;
+              this.currencyCode = bundle.data.formats.currency;
+              this.dateTimeFormat = this.dateFormat + ' ' + this.timeFormat;
+              return bundle;
             })
             .catch(() => {
-                console.warn(`error loading locale resource from ${path}`);
+              console.warn(`error loading locale resource from ${path}`);
             });
+
+        });
     }
 
     setSelectedLocale(locale: string) {
+        const appConfig = injector.get<AppConfig>('APP_CONFIG');
+        let key = 'selectedLocale';
         this.selectedLocale = locale;
-        AsyncStorage.setItem('selectedLocale', locale);
+        if (isWebPreviewMode()) {
+          key = appConfig.appProperties.displayName + '_selectedLocale';
+        }
+        AsyncStorage.setItem(key, locale);
     }
 
     async getSelectedLocale() {
-        return await AsyncStorage.getItem('selectedLocale');
+      const appConfig = injector.get<AppConfig>('APP_CONFIG');
+      const key = isWebPreviewMode() ? appConfig && appConfig.appProperties.displayName + '_selectedLocale' : 'selectedLocale';
+      return await AsyncStorage.getItem(key);
     }
 }
 
