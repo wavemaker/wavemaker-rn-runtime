1c5722783e5b6b62c577342508bb6e11869a4c83
bandhavya <bandhavya.vakiti@wavemaker.com>
2021-12-23 13:22:55 +0530

    Fixes WMS-21583, setting boolean to cancelable prop on wizard

--- src/components/container/wizard/wizard.props.ts
+++ src/components/container/wizard/wizard.props.ts
@@ -3,7 +3,7 @@ import { BaseProps } from '@wavemaker/app-rn-runtime/core/base.component';
 export default class WmWizardProps extends BaseProps {
   actionsalignment: string = 'right';
   children: any;
-  cancelable = true;
+  cancelable: boolean = true;
   cancelbtnlabel: string = 'Cancel';
   donebtnlabel: string = 'Done';
   nextbtnlabel: string = 'Next';
