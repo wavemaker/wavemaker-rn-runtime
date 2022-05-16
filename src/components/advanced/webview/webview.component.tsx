17b279ac31e533e3069e24d2c0ddc22edfb1aced
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2022-05-16 21:29:13 +0530

    WMS-22241: Add webview widget that embeds web component inside app.

--- src/components/advanced/webview/webview.component.tsx
+++ src/components/advanced/webview/webview.component.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { HideMode } from '@wavemaker/app-rn-runtime/core/if.component';
 import { BaseComponent, BaseComponentState } from '@wavemaker/app-rn-runtime/core/base.component';
 
 import WmWebviewProps from './webview.props';
 import { DEFAULT_CLASS, DEFAULT_STYLES, WebviewStyles } from './webview.styles';
 
-export default class WmWebview extends BaseComponent<WmWebviewProps, BaseComponentState<WmWebviewProps>, WebviewStyles> {
+class WmWebViewState extends BaseComponentState<WmWebviewProps> {
+
+}
+
+export default class WmWebview extends BaseComponent<WmWebviewProps, WmWebViewState, WebviewStyles> {
 
   constructor(props: WmWebviewProps) {
     super(props, DEFAULT_CLASS, DEFAULT_STYLES, new WmWebviewProps());
+    this.hideMode = HideMode.DONOT_ADD_TO_DOM;
+  }
+
+  getTitle(iframe: any) {
+    try {
+      return iframe.currentTarget.contentWindow.document.title;
+    } catch(e) {
+      // browser blocks cross origin access to iframe content.
+    }
+  }
+
+  setTitle(title: string) {
+    this.updateState({
+      props: {
+        title: title
+      }
+    } as WmWebViewState);
   }
 
   protected renderWidget(props: WmWebviewProps) {
     return (
-      <View style={this.styles.container}>
-        <WebView 
-          style={this.styles.webview}
-          source={{
-            uri: props.src
-          }}>
-        </WebView>
+      <View style={this.styles.root}>
+        {Platform.OS === 'web' ?
+          (<iframe src={props.src} width={'100%'} height={'100%'} onLoad={(e) => {
+            this.setTitle(this.getTitle(e.currentTarget));
+            this.invokeEventCallback('onLoad', [null, this]);
+          }}></iframe>) : 
+          (<WebView 
+            style={this.styles.webview}
+            source={{
+              uri: props.src
+            }}
+            onLoadEnd={(e) => {
+              this.setTitle(e.nativeEvent.title);
+              this.invokeEventCallback('onLoad', [null, this]);
+            }}>
+          </WebView>)}
       </View>
     );
   }
--- src/components/advanced/webview/webview.props.ts
+++ src/components/advanced/webview/webview.props.ts
@@ -2,4 +2,5 @@ import { BaseProps } from '@wavemaker/app-rn-runtime/core/base.component';
 
 export default class WmWebviewProps extends BaseProps {
   src: string = null as any;
+  title: string = null as any;
 }
