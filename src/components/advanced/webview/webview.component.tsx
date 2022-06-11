1c39440287c9deed3bb2e89ff84035b29be9f45b
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2022-06-11 16:30:55 +0530

    WMS-22431: Add InjectJavascript into webview

--- src/components/advanced/webview/webview.component.tsx
+++ src/components/advanced/webview/webview.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Platform, View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { BackHandler, Platform, View } from 'react-native';
+import { WebView, WebViewNavigation, WebViewMessageEvent } from 'react-native-webview';
 import { HideMode } from '@wavemaker/app-rn-runtime/core/if.component';
 import { BaseComponent, BaseComponentState } from '@wavemaker/app-rn-runtime/core/base.component';
 
@@ -13,9 +13,52 @@ class WmWebViewState extends BaseComponentState<WmWebviewProps> {
 
 export default class WmWebview extends BaseComponent<WmWebviewProps, WmWebViewState, WebviewStyles> {
 
+  private webview: WebView | null = null as any;
+  private webViewState: WebViewNavigation = null as any;
+  private invokeJSCallbacks = {} as any;
+
   constructor(props: WmWebviewProps) {
     super(props, DEFAULT_CLASS, DEFAULT_STYLES, new WmWebviewProps());
     this.hideMode = HideMode.DONOT_ADD_TO_DOM;
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonPress);
+  }
+
+  componentWillUnmount() {
+    super.componentWillUnmount();
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonPress);
+  }
+
+  handleBackButtonPress = (): boolean => {
+    if (this.webview && this.webViewState && this.webViewState.canGoBack) {
+      this.webview.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  injectJavaScript(fn: string) {
+    return new Promise((resolve, reject) => {
+      if (this.webview) {
+        const id = '' + Date.now();
+        this.invokeJSCallbacks[id] = resolve;
+        this.webview.injectJavaScript(
+          `window.ReactNativeWebView.postMessage('afterInjectJavaScript:' + ${id} + ':' + JSON.stringify((${fn})()))`
+        );
+      } else {
+        reject();
+      }
+    });
+  }
+
+  onMessage = (event: WebViewMessageEvent) => {
+    const data: string = event.nativeEvent?.data;
+    if (data && data.startsWith('afterInjectJavaScript')) {
+      const id = data?.match(/\:([0-9]+)\:/);
+      const callback = id && this.invokeJSCallbacks[id[1]];
+      callback && callback(JSON.parse(data.substring(data.indexOf(':', data.indexOf(':') + 1) + 1)));
+    } else {
+      this.invokeEventCallback('onMessage', [event, this]);
+    }
   }
 
   getTitle(iframe: any) {
@@ -40,17 +83,22 @@ export default class WmWebview extends BaseComponent<WmWebviewProps, WmWebViewSt
         {Platform.OS === 'web' ?
           (<iframe src={props.src} width={'100%'} height={'100%'} onLoad={(e) => {
             this.setTitle(this.getTitle(e.currentTarget));
-            this.invokeEventCallback('onLoad', [null, this]);
+            this.invokeEventCallback('onLoad', [e, this]);
           }}></iframe>) :
           (<WebView
+            ref={(ref) => this.webview = ref}
             nestedScrollEnabled={true}
             style={this.styles.webview}
             source={{
               uri: props.src
             }}
+            onMessage={this.onMessage}
+            onNavigationStateChange={(state) => {
+              this.webViewState = state;
+            }}
             onLoadEnd={(e) => {
               this.setTitle(e.nativeEvent.title);
-              this.invokeEventCallback('onLoad', [null, this]);
+              this.invokeEventCallback('onLoad', [e, this]);
             }}>
           </WebView>)}
       </View>
