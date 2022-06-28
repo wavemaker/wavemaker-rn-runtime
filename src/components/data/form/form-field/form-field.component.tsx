87e11a81277152583ceccad231866b4e08b40825
Swathi Anupuram <swathi.anupuram@wavemaker.com>
2022-06-28 13:20:59 +0530

    WMS-22147: Added custom validations for formwidgets

--- src/components/data/form/form-field/form-field.component.tsx
+++ src/components/data/form/form-field/form-field.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import { BaseComponent, BaseComponentState } from '@wavemaker/app-rn-runtime/core/base.component';
 import { widgetsWithUndefinedValue } from '@wavemaker/app-rn-runtime/core/utils';
-import { isEqual, get, cloneDeep, forEach } from 'lodash';
+import { isEqual, get, find, cloneDeep, forEach, keys } from 'lodash';
 
 import WmFormFieldProps from './form-field.props';
 import { DEFAULT_CLASS, DEFAULT_STYLES, WmFormFieldStyles } from './form-field.styles';
@@ -17,11 +17,14 @@ export default class WmFormField extends BaseComponent<WmFormFieldProps, WmFormF
   public formwidget: any;
   public _syncValidators: any;
   public defaultValidatorMessages: any = [];
+  private notifyForFields: any = [];
+  private _asyncValidatorFn: any;
   constructor(props: WmFormFieldProps) {
     super(props, DEFAULT_CLASS, DEFAULT_STYLES, new WmFormFieldProps(), new WmFormFieldState());
   }
 
   onFieldChangeEvt(name: string, $new: any, $old: any, isDefault: boolean) {
+    this.notifyChanges();
     this.validateFormField();
     if (!isEqual($old, $new)) {
       this.updateState({ props: { datavalue: $new }} as WmFormFieldState, () => {
@@ -31,6 +34,59 @@ export default class WmFormField extends BaseComponent<WmFormFieldProps, WmFormF
         this.form.updateDataOutput.call(this.form, get(this.props, 'formKey', this.props.name), $new);
       }
     }
+  }
+
+  // Registers observer of validation fields
+  observeOn(fields: any) {
+    forEach(fields, (field) => {
+      const formfield = find(this.form.formFields, (f) => f.proxy.name === field);
+      if (formfield) {
+        formfield.notifyForFields.push(this);
+      }
+    });
+  }
+
+  // Notifies changes to observing validation fields
+  notifyChanges() {
+    forEach(this.notifyForFields, (field) => {
+      field.formwidget.validate(field.formwidget.datavalue);
+      setTimeout(() => field.validateFormField());
+    });
+  }
+
+  getPromiseList(validators: any) {
+    const arr: any = [];
+    forEach(validators, (fn) => {
+      let promise = fn;
+      if (fn instanceof Function && fn.bind) {
+        promise = fn(this.formwidget.proxy, this.form);
+      }
+      if (promise instanceof Promise) {
+        arr.push(promise);
+      }
+    });
+    return arr;
+  }
+
+  // this method sets the asyncValidation on the form field. Assigns validationmessages from the returned response
+  setAsyncValidators(validators: any) {
+    this._asyncValidatorFn = () => {
+        return Promise.all(this.getPromiseList(validators)).then(() => {
+          return null;
+        }, err => {
+          let validationMsg: string;
+          // if err obj has validationMessage key, then set validationMessage using this value
+          // else return the value of the first key in the err object as validation message.
+          if (err.hasOwnProperty('errorMessage')) {
+            validationMsg = get(err, 'errorMessage');
+          } else {
+            const messageKeys = keys(err);
+            validationMsg = (err[messageKeys[0]]).toString();
+          }
+          this.setInvalidState(validationMsg);
+          return err;
+        })
+      };
   }
 
   // sets the default validation on the form field
@@ -48,15 +104,30 @@ export default class WmFormField extends BaseComponent<WmFormFieldProps, WmFormF
         const key = get(obj, 'type');
         this.defaultValidatorMessages[key] = get(obj, 'errorMessage');
         const value = get(obj, 'validator');
-        let validObj: any = {
+        let propsObj: any = {
           props: {}
-        }
-        validObj.props[key] = value;
-        this.formwidget.updateState(validObj as WmFormFieldState);
+        };
+        propsObj.props[key] = value;
+        key === 'required' && this.updateState(propsObj as WmFormFieldState);
+        this.formwidget.updateState(propsObj as WmFormFieldState);
       }
     });
   }
 
+  setInvalidState(msg: string) {
+    this.updateState({
+      isValid: false,
+      props: {
+        validationmessage: msg
+      }
+    } as WmFormFieldState)
+    this.formwidget.updateState({
+      isValid: false,
+      props: {
+        validationmessage: msg
+      }
+    } as WmFormFieldState);
+  }
 
   onPropertyChange(name: string, $new: any, $old: any) {
     switch (name) {
@@ -99,20 +170,11 @@ export default class WmFormField extends BaseComponent<WmFormFieldProps, WmFormF
           // passing formwidget and form as arguments to the errorMessage function.
           validationMsg = validationMsg(this.formwidget.proxy, this.form);
         }
-        this.updateState({
-          isValid: false,
-          props: {
-            validationmessage: validationMsg
-          }
-        } as WmFormFieldState)
-        this.formwidget.updateState({
-          isValid: false,
-          props: {
-            validationmessage: validationMsg
-          }
-        } as WmFormFieldState);
+        this.setInvalidState(validationMsg);
       }
     })
+
+    this._asyncValidatorFn && this._asyncValidatorFn();
 
   }
 
