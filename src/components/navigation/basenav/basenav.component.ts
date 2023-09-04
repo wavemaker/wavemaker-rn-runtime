8b2418feb2fe61781d18fc79f5e92797bcefaa19
abdullahk_500335 <abdullahk_500335@WMHYLTAP035.local>
2023-09-04 13:20:41 +0530

    WMS-23269 there is a icon field for dataset type string which is not necessary hence causing the issue

--- src/components/navigation/basenav/basenav.component.ts
+++ src/components/navigation/basenav/basenav.component.ts
@@ -41,8 +41,6 @@ export abstract class BaseNavComponent< T extends BaseNavProps, S extends BaseNa
         return {
           key: `${name}_item${i}`,
           label: s,
-          data: s,
-          icon: 'wi wi-' + s
         } as NavigationDataItem;
       });
     } else if (dataset) {
