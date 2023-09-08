d25fa417c4dffb18093effd3b70c822c18fa7a57
Swathi Anupuram <swathi.anupuram@wavemaker.com>
2023-09-08 19:08:52 +0530

    WMS-24139[React Native]: Added Horizontal Stack Bar and Arc chats

---
+++ src/components/chart/stack-chart/stack-chart.styles.ts
@@ -0,0 +1,10 @@
+import BASE_THEME from '@wavemaker/app-rn-runtime/styles/theme';
+import { BaseStyles, defineStyles } from '@wavemaker/app-rn-runtime/core/base.component';
+import {BaseChartComponentStyles, DEFAULT_CLASS as BASE_CHART_DEFAULT_CLASS } from '../basechart.styles';
+
+export type WmStackChartStyles = BaseStyles & BaseChartComponentStyles;
+
+export const DEFAULT_CLASS = 'app-stack-chart';
+BASE_THEME.registerStyle((themeVariables, addStyle) => {
+  addStyle(DEFAULT_CLASS, BASE_CHART_DEFAULT_CLASS, {});
+});
