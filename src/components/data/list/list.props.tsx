940b4a010c54d40459612e7b519d95d58f064689
Swathi Anupuram <swathi.anupuram@wavemaker.com>
2024-01-08 19:31:32 +0530

    WMS-24977[React Native]: Changes related to form in list scenarios

--- src/components/data/list/list.props.tsx
+++ src/components/data/list/list.props.tsx
@@ -3,6 +3,7 @@ import { BaseProps } from '@wavemaker/app-rn-runtime/core/base.component';
 import { View } from 'react-native';
 
 export default class WmListProps extends BaseProps {
+    formRef: any;
     iconclass: string = null as any;
     title: string = null as any;
     subheading: string = null as any;
