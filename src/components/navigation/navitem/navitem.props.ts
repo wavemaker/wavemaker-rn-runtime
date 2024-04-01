524805fce990f47e9d0d39e7bb2df5b3184d991a
Mak <abdullah.mohammed@wavemaker.com>
2024-04-01 10:25:57 +0530

    WMS-25468 triger onTap event of parent when child is invoked, rename TapEvent to SyntheticEvent

--- src/components/navigation/navitem/navitem.props.ts
+++ src/components/navigation/navitem/navitem.props.ts
@@ -1,6 +1,6 @@
 import {BaseComponent, BaseProps} from '@wavemaker/app-rn-runtime/core/base.component';
 import {NavigationDataItem} from "@wavemaker/app-rn-runtime/components/navigation/basenav/basenav.component";
-import {TapEvent} from "@wavemaker/app-rn-runtime/core/tappable.component";
+import {SyntheticEvent} from "@wavemaker/app-rn-runtime/core/tappable.component";
 import { AccessibilityRole } from 'react-native';
 
 export default class WmNavItemProps extends BaseProps {
@@ -8,7 +8,7 @@ export default class WmNavItemProps extends BaseProps {
   caption?: string;
   item: any = [];
   view: 'default' | 'dropdown' | 'anchor' = 'default';
-  onSelect? = ($event: TapEvent, target: any, $item: NavigationDataItem) => {};
+  onSelect? = ($event: SyntheticEvent, target: any, $item: NavigationDataItem) => {};
   getDisplayExpression? = (label: string) => null as any;
   accessibilitylabel?: string = undefined;
   hint?: string = undefined;
