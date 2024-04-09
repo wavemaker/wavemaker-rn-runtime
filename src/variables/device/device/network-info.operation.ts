d7529b945856d41104a89a1017a1f7500085ae4b
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2024-04-09 10:35:04 +0530

    WMS-25516: onOnline and onOffline events are not getting called
    (cherry picked from commit 90856ebc5358c8fcaf7a99c957dc02c4f40b8269)

--- src/variables/device/device/network-info.operation.ts
+++ src/variables/device/device/network-info.operation.ts
@@ -1,20 +1,41 @@
 import * as Network from 'expo-network';
 import { Operation } from '@wavemaker/app-rn-runtime/variables/device/operation.provider';
+import NetworkService from '@wavemaker/app-rn-runtime/core/network.service';
+
+import { DeviceVariable } from '../../device-variable';
 
 export interface NetworkInfoOutput {
   connectionType: string;
+  isConnecting: boolean;
   isNetworkAvailable: boolean;
   isOnline: boolean;
+  isOffline: boolean;
 }
 
 export class NetworkInfoOperation implements Operation {
-  public invoke(): Promise<NetworkInfoOutput> {
+  isOnline = true;
+
+  public invoke(params?: any, onSuccess?: Function, onError?: Function, operation?: string, variable?: DeviceVariable): Promise<NetworkInfoOutput> {
+    const networkState = params.networkStatus;
     return Network.getNetworkStateAsync().then((response) => {
       return {
-        connectionType: Network.NetworkStateType[response.type as Network.NetworkStateType],
-        isNetworkAvailable: response.isInternetReachable,
-        isOnline: response.isInternetReachable
+        connectionType: Network.NetworkStateType[response.type as Network.NetworkStateType],      
+        isConnecting: networkState.isConnecting,
+        isNetworkAvailable: networkState.isNetworkAvailable,
+        isOnline: networkState.isConnected,
+        isOffline: !networkState.isConnected
       } as NetworkInfoOutput;
+    }).then((dataset) => {
+      try {
+        if (this.isOnline !== networkState.isConnected) {
+          this.isOnline = networkState.isConnected;
+          const callback = (variable?.config as any)[networkState.isConnected ? 'onOnline' : 'onOffline'] as Function;
+          callback?.(variable, dataset);
+        }
+      } catch(e) {
+        console.error(e);
+      }
+      return dataset;
     });
   }
 }
