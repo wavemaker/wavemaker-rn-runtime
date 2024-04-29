53ea8f8a0b06fbe7dda7658feda9dca2c00730fd
Swathi Anupuram <swathi.anupuram@wavemaker.com>
2024-04-29 15:35:29 +0530

    WMS-25611: [React Native] OnBeforeservicecall event is not triggering for liveform widget
    (cherry picked from commit 2de053994004256a4470820ae2b977e4fd1ec0bf)

--- src/components/data/form/form.props.ts
+++ src/components/data/form/form.props.ts
@@ -3,6 +3,7 @@ import { BaseProps } from '@wavemaker/app-rn-runtime/core/base.component';
 export default class WmFormProps extends BaseProps {
   children: any;
   dataoutput: any;
+  onBeforeservicecall: any;
   onBeforesubmit: any;
   formdata: any;
   parentForm: string = '';
