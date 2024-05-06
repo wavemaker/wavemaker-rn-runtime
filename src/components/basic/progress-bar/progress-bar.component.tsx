8eef79c530559083c8071acfe6815aac5f9bc773
Vijay Tiwari <vijay.tiwari@wavemaker.com>
2024-05-06 18:32:35 +0530

    Feat: linear gradient support for progress bar ~ WMS-25601

--- src/components/basic/progress-bar/progress-bar.component.tsx
+++ src/components/basic/progress-bar/progress-bar.component.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import { ProgressBar } from 'react-native-paper';
+import { LinearGradient as ExpoLinearGradient } from 'expo-linear-gradient';
 import { Tappable } from '@wavemaker/app-rn-runtime/core/tappable.component';
 import { BaseComponent, BaseComponentState } from '@wavemaker/app-rn-runtime/core/base.component';
 
 import WmProgressBarProps from './progress-bar.props';
 import { DEFAULT_CLASS, WmProgressBarStyles } from './progress-bar.styles';
+import { parseLinearGradient } from '@wavemaker/app-rn-runtime/core/utils';
 
 export class WmProgressBarState extends BaseComponentState<WmProgressBarProps> {}
 
@@ -18,6 +20,10 @@ export default class WmProgressBar extends BaseComponent<WmProgressBarProps, WmP
   renderWidget(props: WmProgressBarProps) {
     let value = (props.datavalue - props.minvalue) / (props.maxvalue - props.minvalue);
     const styles = this.theme.mergeStyle(this.theme.getStyle(`app-${props.type}-progress-bar`), this.styles);
+    const {hasLinearGradient, color1, color2, start, end} = parseLinearGradient(styles?.root?.progressBar?.backgroundColor as string);
+    const gradientColors = [color1, color2];
+    const valuePercent = `${Math.round(value * 100)}%`;
+
     return (
     <View style={styles.root}>
       {this._background}
@@ -27,6 +33,23 @@ export default class WmProgressBar extends BaseComponent<WmProgressBarProps, WmP
           progress={value}
           color={styles.progressValue.color}
           style={[styles.progressBar, {height: styles.root.height || styles.progressBar.height}]}></ProgressBar>
+          {hasLinearGradient ? (
+            <ExpoLinearGradient
+              colors={gradientColors}
+              start={start}
+              end={end}
+              style={[
+                {
+                  width: valuePercent as any,
+                  height: styles.root.height || styles.progressBar.height,
+                  position: 'absolute',
+                  borderRadius: styles?.progressBar?.borderRadius || 0,
+                },
+              ]}
+            />
+          ) : (
+            <></>
+          )}
       </Tappable>
     </View>); 
   }
