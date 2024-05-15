27720cb0a471db35590802fa38cf25245594b298
Swathi Anupuram <swathi.anupuram@wavemaker.com>
2024-05-15 22:07:07 +0530

    WMS-25421: Resolving build failure issue

--- src/components/input/checkboxset/checkboxset.component.tsx
+++ src/components/input/checkboxset/checkboxset.component.tsx
@@ -53,7 +53,7 @@ export default class WmCheckboxset extends BaseDatasetComponent<WmCheckboxsetPro
     return (
       <TouchableOpacity {...this.getTestPropsForAction(index + '')}
         style={[this.styles.item, {width: colWidth}]}
-        onPress={this.onPress.bind(this, item)} key={item.key} {...getAccessibilityProps(AccessibilityWidgetType.CHECKBOX, {...props, checked: item.selected})} accessibilityRole='checkbox' accessibilityLabel={`Checkbox for ${displayText}`}>
+        onPress={this.onPress.bind(this, item)} key={item.key}>
         <WmIcon iconclass="wi wi-check" styles={item.selected? this.styles.checkicon : this.styles.uncheckicon} disabled={props.readonly || props.disabled}/>
         <Text {...this.getTestPropsForLabel(index + '')} style={this.styles.text}>{displayText}</Text>
       </TouchableOpacity>)
