00e18dc1e143c45f0eeb7675dc920e8b505c2547
raajvamsy <raajvamsy.rayavarapu@wavemaker.com>
2024-05-30 22:25:57 +0530

    WMS-25768 Style Changes implemented in FIS - Fixed Button Styles

--- src/components/basic/button/button.component.tsx
+++ src/components/basic/button/button.component.tsx
@@ -71,7 +71,8 @@ export default class WmButton extends BaseComponent<WmButtonProps, WmButtonState
             paddingLeft: 0,
             paddingRight: 0,
             overflow: 'hidden',
-            flexDirection: 'column'
+            flexDirection: 'column',
+            flex: 1
           }
         ]}
         // accessibilityProps={{...getAccessibilityProps(
@@ -87,6 +88,7 @@ export default class WmButton extends BaseComponent<WmButtonProps, WmButtonState
             paddingLeft: this.styles.root.paddingLeft,
             paddingRight: this.styles.root.paddingRight,
             width: '100%',
+            height: this.styles.root.height ? '100%' : null,
             justifyContent: 'center',
           }}
           rippleColor = {this.styles.root.rippleColor}
