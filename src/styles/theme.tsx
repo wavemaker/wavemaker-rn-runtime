d990f817fd3cb254045c619f8b931f1bc52eb96a
Mak <abdullah.mohammed@wavemaker.com>
2024-06-22 22:48:58 +0530

    WMS-25657 support for vw and vh units

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -172,7 +172,7 @@ export class Theme {
 
     cleanseStyleProperties(style: any) {
         if (!(style && isObject(style)) || isString(style) || isArray(style)) {
-            return;
+            return style;
         }
         style = style as any;
         if (isObject(style) && !isArray(style)) {
@@ -202,6 +202,7 @@ export class Theme {
             delete style['padding'];
         }
         Object.keys(style).forEach((k, i) => this.cleanseStyleProperties(style[k]));
+        return style;
     }
 
     getStyle(name: string) {
