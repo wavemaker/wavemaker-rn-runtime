cab0ac7d710e2d715a8193b61175d8cac178dcab
vikas.pinnapu <vikas.pinnapu@wavemaker.com>
2024-07-09 20:50:46 +0530

    WMS-26026:video and checkbox component unit tests

--- test/components/basic/video.component.spec.tsx
+++ test/components/basic/video.component.spec.tsx
@@ -1,12 +1,165 @@
-import React, { ReactNode } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import React, { ReactNode, createRef } from 'react';
+import {
+  render,
+  screen,
+  fireEvent,
+  act,
+  waitFor,
+} from '@testing-library/react-native';
 import WmVideo from '@wavemaker/app-rn-runtime/components/basic/video/video.component';
+import WmVideoProps from '../../../src/components/basic/video/video.props';
+import { AssetConsumer, AssetProvider } from '../../../src/core/asset.provider';
+import { Video } from 'expo-av';
+
+// Mock Expo Video component
+// jest.mock('expo-av', () => ({
+//   Video: jest.fn((props) => <div {...props} />),
+// }));
+
+const loadAsset = (path) => path;
+const renderComponent = (props = {}) =>
+  render(
+    <AssetProvider value={loadAsset}>
+      <WmVideo {...props} />
+    </AssetProvider>
+  );
 
 describe('Test Video component', () => {
-    test('Check validity of sample component', () => {
-      const tree = renderer.create(<WmVideo name="test_Video"/>).toJSON();
-      expect(tree).toMatchSnapshot();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders component with default props', () => {
+    render(
+      <AssetProvider value={loadAsset}>
+        <WmVideo />
+      </AssetProvider>
+    );
+    expect(screen).toMatchSnapshot();
+  });
+
+  // Source URL Handling
+  it('handles different mp4 video source URL correctly', async () => {
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
     });
+
+    const video = screen.getByTestId('test_video');
+    await waitFor(() => {
+      expect(video.props.source).toMatchObject({
+        uri: 'https://example.com/video.mp4',
+      });
+    });
+  });
+
+  it('handles different webm video source URLs correctly', async () => {
+    renderComponent({
+      name: 'test',
+      webmformat: 'https://example.com/video.webm',
+    });
+
+    const video = screen.getByTestId('test_video');
+
+    await waitFor(() => {
+      expect(video.props.source).toMatchObject({
+        uri: 'https://example.com/video.webm',
+      });
+    });
+  });
+
+  // Autoplay Functionality
+  it('autoplays the video when autoplay is true', () => {
+    const componentDidMountSpy = jest.spyOn(
+      WmVideo.prototype,
+      'componentDidMount'
+    );
+    const playAsyncMock = jest.spyOn(Video.prototype, 'playAsync');
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
+      autoplay: true,
+    });
+    const video = screen.getByTestId('test_video');
+    // expect(Video).toHaveBeenCalledWith(
+    //   expect.objectContaining({
+    //     shouldPlay: true,
+    //   }),
+    //   {}
+    // );
+    expect(componentDidMountSpy).toHaveBeenCalled();
+    expect(playAsyncMock).toHaveBeenCalled();
+    componentDidMountSpy.mockRestore();
+  });
+
+  // Accessibility Props
+  it('applies accessibility props correctly', () => {
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
+      accessibilitylabel: 'Accessible Video',
+      hint: 'video',
+      accessibilityrole: 'Video',
+    });
+    expect(screen.getByLabelText('Accessible Video')).toBeTruthy();
+    expect(screen.getByRole('Video')).toBeTruthy();
+    expect(screen.getByA11yHint('video')).toBeTruthy();
+  });
+
+  it('shows controls when controls prop is true', () => {
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
+      accessibilitylabel: 'Accessible Video',
+      hint: 'video',
+      accessibilityrole: 'Video',
+      controls: true,
+    });
+    const video = screen.getByTestId('test_video');
+    expect(video.props.useNativeControls).toBe(true);
+  });
+
+  // Loop Prop Functionality
+  it('loops the video when loop prop is true', () => {
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
+      accessibilitylabel: 'Accessible Video',
+      hint: 'video',
+      accessibilityrole: 'Video',
+      loop: true,
+    });
+    const video = screen.getByTestId('test_video');
+    expect(video.props.status.isLooping).toBe(true);
+  });
+
+  // Mute Prop Functionality
+  it('mutes the video when muted prop is true', () => {
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
+      accessibilitylabel: 'Accessible Video',
+      hint: 'video',
+      accessibilityrole: 'Video',
+      muted: true,
+    });
+    const video = screen.getByTestId('test_video');
+    expect(video.props.status.isMuted).toBe(true);
+  });
+
+  // Poster Source
+  it('sets poster image correctly', () => {
+    renderComponent({
+      name: 'test',
+      mp4format: 'https://example.com/video.mp4',
+      videoposter: 'https://example.com/poster.png',
+      accessibilitylabel: 'Accessible Video',
+      hint: 'video',
+      accessibilityrole: 'Video',
+    });
+    const video = screen.root.children[1];
+    expect(video.props.posterSource).toEqual({
+      uri: 'https://example.com/poster.png',
+    });
+  });
 });
