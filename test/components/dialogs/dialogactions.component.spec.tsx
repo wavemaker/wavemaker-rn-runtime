4665009a042359ffe8aafdd74715a1a2352b910a
@jyotidas.ankur <jyotidas.ankur@wavemaker.com>
2024-07-18 13:59:15 +0530

    WMS-26026 | add: test cases for dialogcontent and dialogactions component

--- test/components/dialogs/dialogactions.component.spec.tsx
+++ test/components/dialogs/dialogactions.component.spec.tsx
@@ -1,12 +1,53 @@
-import React, { ReactNode } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import React from 'react';
+import { Text } from 'react-native';
+import { render } from '@testing-library/react-native';
 import WmDialogactions from '@wavemaker/app-rn-runtime/components/dialogs/dialogactions/dialogactions.component';
 
-describe('Test Dialogactions component', () => {
-    test('Check validity of sample component', () => {
-      const tree = renderer.create(<WmDialogactions name="test_Dialogactions"/>).toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+describe('Dialogactions component', () => {
+  test('should render component', () => {
+    const tree = render(<WmDialogactions name="test_Dialogactions" />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should render component with children', () => {
+    const tree = render(
+      <WmDialogactions name="test_Dialogactions">
+        <Text>Test children component</Text>
+      </WmDialogactions>
+    );
+
+    expect(tree.getByText('Test children component')).toBeDefined();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should render component with custom style', () => {
+    const tree = render(
+      <WmDialogactions
+        name="test_Dialogactions"
+        styles={{
+          root: {
+            borderColor: '#404040',
+          },
+        }}
+      >
+        <Text>Test children component</Text>
+      </WmDialogactions>
+    );
+
+    expect(tree.getByText('Test children component')).toBeDefined();
+    expect(tree.toJSON().props.style).toMatchObject({ borderColor: '#404040' });
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should not show component when show is false', () => {
+    const tree = render(
+      <WmDialogactions name="test_Dialogactions" show={false}>
+        <Text>Test children component</Text>
+      </WmDialogactions>
+    );
+
+    expect(tree.toJSON().props.style).toMatchObject({ height: 0, width: 0 });
+    expect(tree).toMatchSnapshot();
+  });
 });
--- test/components/dialogs/dialogcontent.component.spec.tsx
+++ test/components/dialogs/dialogcontent.component.spec.tsx
@@ -1,12 +1,89 @@
-import React, { ReactNode } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import React from 'react';
+import {Text} from 'react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import WmDialogcontent from '@wavemaker/app-rn-runtime/components/dialogs/dialogcontent/dialogcontent.component';
 
 describe('Test Dialogcontent component', () => {
-    test('Check validity of sample component', () => {
-      const tree = renderer.create(<WmDialogcontent name="test_Dialogcontent"/>).toJSON();
+    test('should render component', () => {
+      const tree = render(<WmDialogcontent name="test_Dialogcontent"/>);
+
       expect(tree).toMatchSnapshot();
     });
+
+    test('should render component with children', () => {
+      const tree = render(
+        <WmDialogcontent name="test_Dialogcontent">
+          <Text>Test Children component</Text>
+        </WmDialogcontent>
+      );
+
+      expect(tree.getByText("Test Children component")).toBeDefined();
+      expect(tree).toMatchSnapshot();
+    })
+
+    test('should render component with custom style', () => {
+      const tree = render(
+        <WmDialogcontent 
+          name="test_Dialogcontent"
+          styles={{
+            backgroundColor: '#040404',
+            maxHeight: 400
+          }}
+        >
+          <Text>Test Children component</Text>
+        </WmDialogcontent>
+      );
+
+      const containerStyleArray = tree.toJSON().props.contentContainerStyle;
+      const containerStyle = {};
+      containerStyleArray.forEach(item => {
+        if(!item) return;
+        Object.keys(item).forEach(key => {
+          containerStyle[key] = item[key];
+        })
+      })
+
+      expect(containerStyle).toMatchObject({backgroundColor: '#040404', maxHeight: 400})
+      expect(tree.toJSON().props.style).toMatchObject({maxHeight: 400});
+      expect(tree).toMatchSnapshot();
+    })
+
+    test('should not show component when show is false', () => {
+      const tree = render(
+        <WmDialogcontent 
+          name="test_Dialogcontent"
+          show={false}
+        >
+          <Text>Test Children component</Text>
+        </WmDialogcontent>
+      );
+      const containerStyleArray = tree.toJSON().props.contentContainerStyle;
+      const containerStyle = {};
+      containerStyleArray.forEach(item => {
+        if(!item) return;
+        Object.keys(item).forEach(key => {
+          containerStyle[key] = item[key];
+        })
+      })
+
+      expect(containerStyle).toMatchObject({height: 0, width: 0})
+      expect(tree).toMatchSnapshot();
+    })
+
+    test('should call notify method when component is scrolled', () => {
+      const notifyMock = jest.spyOn(WmDialogcontent.prototype, 'notify');
+      const tree = render(
+        <WmDialogcontent 
+          name="test_Dialogcontent"
+          show={false}
+        >
+          <Text>Test Children component</Text>
+        </WmDialogcontent>
+      );
+
+      fireEvent(tree.root, 'scroll');
+
+      expect(notifyMock).toHaveBeenCalled();
+      expect(notifyMock).toHaveBeenCalledWith('scroll', expect.arrayContaining([]))
+    })
 });
