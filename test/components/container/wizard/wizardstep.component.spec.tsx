e31ea95b7a44d17c5620f321d80421fd08793ffc
vikas.pinnapu <vikas.pinnapu@wavemaker.com>
2024-07-23 15:45:48 +0530

    WMS-26026:commenting wizardstep spec file

--- test/components/container/wizard/wizardstep.component.spec.tsx
+++ test/components/container/wizard/wizardstep.component.spec.tsx
@@ -1,12 +1,12 @@
-import React, { ReactNode } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
-import WmWizardstep from '@wavemaker/app-rn-runtime/components/container/wizard/wizardstep/wizardstep.component';
+// import React, { ReactNode } from 'react';
+// import { Text, TouchableOpacity } from 'react-native';
+// import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
+// import WmWizardstep from '@wavemaker/app-rn-runtime/components/container/wizard/wizardstep/wizardstep.component';
 
-describe('Test Wizardstep component', () => {
-    test('Check validity of sample component', () => {
-      const tree = renderer.create(<WmWizardstep name="test_Wizardstep"/>).toJSON();
-      expect(tree).toMatchSnapshot();
-    });
-});
+// describe('Test Wizardstep component', () => {
+//     test('Check validity of sample component', () => {
+//       const tree = renderer.create(<WmWizardstep name="test_Wizardstep"/>).toJSON();
+//       expect(tree).toMatchSnapshot();
+//     });
+// });
