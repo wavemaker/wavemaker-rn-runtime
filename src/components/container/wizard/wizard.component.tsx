7efe4e123570ebe8b215cd6bb5a0b94367c1f0d2
premk_500362 <premkumar.katta@wavemaker.com>
2024-09-27 15:20:04 +0530

    Fix: WMS-26596 - Wizard Stepcount not displaying for defaultStep none

--- src/components/container/wizard/wizard.component.tsx
+++ src/components/container/wizard/wizard.component.tsx
@@ -58,7 +58,7 @@ export default class WmWizard extends BaseComponent<WmWizardProps, WmWizardState
 
   componentDidMount() {
     super.componentDidMount();
-    if (this.props.defaultstep) {
+    if (this.props.defaultstep && this.props.defaultstep != 'none') {
       this.updateDefaultStep();
     }
   }
@@ -158,6 +158,7 @@ export default class WmWizard extends BaseComponent<WmWizardProps, WmWizardState
     const isFirstStep = index === 0;
     const isActiveStep = index === this.state.currentStep;
     const isNumberTextLayout = this.state.props.classname === 'number-text-inline';
+    const wizardStepCountVisibility = (index >= this.state.currentStep && !this.state.isDone) || !this.state.currentStep
     return item.state.props.show != false ? (
       <View style={[this.styles.headerWrapper, isNumberTextLayout ?
         {paddingRight: isActiveStep ? 0 : 5, paddingLeft: index === this.state.currentStep + 1 ? 0 : 5}: {}]} key={index+1}>
@@ -165,7 +166,7 @@ export default class WmWizard extends BaseComponent<WmWizardProps, WmWizardState
                           onPress={this.updateCurrentStep.bind(this, index, false)} disabled={index >= this.state.currentStep || !this.state.props.headernavigation}
                           accessibilityRole='header'>
             <View style={this.getStepStyle(index)} {...this.getTestPropsForAction('step'+index)}>
-              {index >= this.state.currentStep && !this.state.isDone &&
+              { wizardStepCountVisibility &&
                 <Text style={isActiveStep ? [this.styles.activeStep, this.styles.activeStepCounter] : this.styles.stepCounter} {...this.getTestPropsForLabel('step' + (index + 1) + '_indicator')}>{index+1}</Text>}
               {(index < this.state.currentStep || this.state.isDone) &&
                 <WmIcon id={this.getTestId('status')} styles={merge({}, this.styles.stepIcon, {icon: {color: this.styles.activeStep.color}})}
