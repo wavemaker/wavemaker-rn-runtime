8c54f3c9012e2d1bed9f92e506a1b75f32ba988c
@jyotidas.ankur <jyotidas.ankur@wavemaker.com>
2024-10-10 13:12:49 +0530

    WMS-26669 | update: prop name in textinput component for autoCapitalization

--- src/core/components/textinput.component.tsx
+++ src/core/components/textinput.component.tsx
@@ -50,7 +50,7 @@ export const WMTextInput = React.forwardRef((props: (TextInputProps &
     activeFloatingLabelStyle: TextStyle,
     customDisplayValue?: string,
     isInputFocused: boolean
-    autocapitalize?: string
+    autoCapitalize?: string
   }), 
     ref: ForwardedRef<TextInput>) => {
     const [selectRange, setSelectRange] = useState<SelectRange>(null as any);
@@ -190,7 +190,7 @@ export const WMTextInput = React.forwardRef((props: (TextInputProps &
             onChange : () => {}
           }: {}}
           contextMenuHidden={!props.allowContentSelection}
-          autoCapitalize={props.autocapitalize}
+          autoCapitalize={props.autoCapitalize}
         ></TextInput>
         {
           hideInput ? (
