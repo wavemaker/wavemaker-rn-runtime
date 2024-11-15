25e2f2f9041f0aac1024e648c673d611a03eccc2
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2024-11-15 11:33:02 +0530

    WMS-26967: Added wm-custom-widget

---
+++ src/components/advanced/customwidgetcontainer/customwidgetcontainer.props.ts
@@ -0,0 +1,5 @@
+import { BaseProps } from "@wavemaker/app-rn-runtime/core/base.component";
+
+export default class WmCustomWidgetContainerProps extends BaseProps {
+    children: any[] = null as any;
+}
