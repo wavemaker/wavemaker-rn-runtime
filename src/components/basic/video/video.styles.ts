86b3dcdca7fbadf8de43a7df26e8022106e542b8
SUNEELGAVARAWM <sunil.gavara@wavemaker.com>
2025-02-01 19:17:41 +0530

    WMS-27478: make the videoposter optional

--- src/components/basic/video/video.styles.ts
+++ src/components/basic/video/video.styles.ts
@@ -1,9 +1,12 @@
 import BASE_THEME from '@wavemaker/app-rn-runtime/styles/theme';
 import { BaseStyles, defineStyles } from '@wavemaker/app-rn-runtime/core/base.component';
 import { WmSkeletonStyles } from '../skeleton/skeleton.styles';
+import { WmPictureStyles } from '../picture/picture.styles';
+import { ViewStyle } from 'react-native';
 
 export type WmVideoStyles = BaseStyles & {
     skeleton: WmSkeletonStyles;
+    playIconContainer: ViewStyle;
 };
 
 export const DEFAULT_CLASS = 'app-video';
@@ -19,7 +22,20 @@ BASE_THEME.registerStyle((themeVariables, addStyle) => {
                 height: 300,
                 width : "100%"    
             }
-        } as any as WmSkeletonStyles
+        } as any as WmSkeletonStyles,
+        playIconContainer: {
+            position: 'absolute', 
+            top: 0, 
+            bottom: 0, 
+            left: 0, 
+            right: 0, 
+            height: '100%', 
+            width: '100%',
+            backgroundColor: 'rgba(0,0,0,0.4)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        } 
     });
     addStyle(DEFAULT_CLASS, '', defaultStyles);
 });
