27834501681a3185a7a6e62174d5eec7920b4cd2
vikas.pinnapu <vikas.pinnapu@wavemaker.com>
2025-02-18 13:06:57 +0530

    WMS-27589|Feat. Selectable date range handling for date widget

--- src/components/input/epoch/wheel-date-picker.component.tsx
+++ src/components/input/epoch/wheel-date-picker.component.tsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import { PixelRatio, StyleSheet, View } from 'react-native';
 import WmWheelScrollPicker from './wheelpicker/wheelpicker.component'
-import { getDateObject, getDates, getMonths, getYearRange } from '@wavemaker/app-rn-runtime/core/utils';
+import { getDateObject, getDates, getMonths, getYearRange, getDaysInMonth, getMonthIndex } from '@wavemaker/app-rn-runtime/core/utils';
+import moment from 'moment';
 
 export class WmWheelDatePickerProps {
+  minDate?: Date | string;
+  maxDate?: Date | string;
   selectedDate?: Date = new Date();
   onDateChange?: (date: Date) => void;
   itemHeight?: number = PixelRatio.roundToNearestPixel(50);
   wrapperHeight?: number = PixelRatio.roundToNearestPixel(150);
 }
 
-export class WmWheelDatePickerState {}
+
+export const START_DATE = {
+  'date': 1,
+  'month': 0,
+  'year': 1950,
+};
+
+export const END_DATE = {
+  'date': 31,
+  'month': 11,
+  'year': 2060,
+};
+
+
+export class WmWheelDatePickerState { }
 
 export class WmWheelDatePicker extends Component<
   WmWheelDatePickerProps,
@@ -22,79 +39,197 @@ export class WmWheelDatePicker extends Component<
   selectedDateIndex: number = 0;
   selectedMonthIndex: number = 0;
   selectedYearIndex: number = 0;
+  defaultSelectedDate: Date;
+  localSelectedDate: Date | string = '';
+  monthValue: number = 0;
+  yearValue: string = '';
+  monthData: string[] = [];
+  yearData: number[] = [];
+  dateData: number[] = [];
+
+
+
 
   constructor(props: WmWheelDatePickerProps) {
     super(props);
-    const selectedDate = this.props.selectedDate || new Date();
+    // const selectedDate = this.props.selectedDate || new Date();
+    const minDate = this.props.minDate ? new Date(this.props.minDate) : new Date(START_DATE.year, START_DATE.month, START_DATE.date);
+    const maxDate = this.props.maxDate ? new Date(this.props.maxDate) : new Date(END_DATE.year, END_DATE.month, END_DATE.date);
+    let selectedDate = this.props.selectedDate ? new Date(this.props.selectedDate) : new Date();
+
+    if (selectedDate < minDate) selectedDate = minDate;
+    if (selectedDate > maxDate) selectedDate = maxDate;
 
     this.itemHeight =
-    PixelRatio.roundToNearestPixel(this.props.itemHeight || 50) || PixelRatio.roundToNearestPixel(50);
+      PixelRatio.roundToNearestPixel(this.props.itemHeight || 50) || PixelRatio.roundToNearestPixel(50);
     this.wrapperHeight =
-    PixelRatio.roundToNearestPixel(this.props.wrapperHeight || 150) || PixelRatio.roundToNearestPixel(150);
-    this.selectedDateIndex = selectedDate.getDate() - 1;
-    this.selectedMonthIndex = selectedDate.getMonth();
-    this.selectedYearIndex = getYearRange().indexOf(selectedDate.getFullYear());
+      PixelRatio.roundToNearestPixel(this.props.wrapperHeight || 150) || PixelRatio.roundToNearestPixel(150);
+
+    // * Initial values
+    this.defaultSelectedDate = selectedDate;
+    this.localSelectedDate = selectedDate;
+    this.monthValue = selectedDate.getMonth();
+    this.yearValue = selectedDate.getFullYear()?.toString();
+
+    const selectedYear = selectedDate.getFullYear();
+    const selectedMonth = selectedDate.getMonth();
+    const selectedDay = selectedDate.getDate();
+
+    // Generate valid year range
+    const minYear = minDate.getFullYear();
+    const maxYear = maxDate.getFullYear();
+    this.yearData = getYearRange(minYear, maxYear);
+    this.selectedYearIndex = this.yearData.indexOf(selectedYear);
+
+    // Generate valid months based on selected year
+    let minMonth = 0;
+    let maxMonth = 11;
+    if (selectedYear === minYear) minMonth = minDate.getMonth();
+    if (selectedYear === maxYear) maxMonth = maxDate.getMonth();
+    this.monthData = getMonths(minMonth, maxMonth);
+    this.selectedMonthIndex = this.monthData.indexOf(getMonths(0, 11)[selectedMonth]);
+
+    // Generate valid days based on selected year & month
+    let minDay = 1;
+    let maxDay = getDaysInMonth(selectedMonth + 1, selectedYear);
+    if (selectedYear === minYear && selectedMonth === minMonth) minDay = minDate.getDate();
+    if (selectedYear === maxYear && selectedMonth === maxMonth) maxDay = maxDate.getDate();
+    this.dateData = getDates(minDay, maxDay);
+    this.selectedDateIndex = this.dateData.indexOf(selectedDay);
   }
 
-  handleValueChange = (
-    valueType: 'date' | 'month' | 'year',
-    value: string | number,
-    index: number,
-  ) => {
+  getMinValue = (type: 'date' | 'month' | 'year') => {
+    if (!this.props.minDate) return START_DATE[type];
+
+    const minDate = new Date(this.props.minDate);
+    switch (type) {
+      case 'date':
+        return minDate.getDate();
+      case 'month':
+        return minDate.getMonth();
+      case 'year':
+        return minDate.getFullYear();
+      default:
+        return 0;
+    }
+  };
+
+  getMaxValue = (type: 'date' | 'month' | 'year') => {
+    if (!this.props.maxDate) return END_DATE[type];
+
+    const maxDate = new Date(this.props.maxDate);
+    switch (type) {
+      case 'date':
+        return maxDate.getDate();
+      case 'month':
+        return maxDate.getMonth();
+      case 'year':
+        return maxDate.getFullYear();
+      default:
+        return 0;
+    }
+  };
+
+
+  handleValueChange = (valueType: 'date' | 'month' | 'year', value: string | number, index: number) => {
+    let indexValue = Object.is(index, -0) ? 0 : index;
     switch (valueType) {
       case 'date':
-        this.selectedDateIndex = index;
+        this.selectedDateIndex = indexValue;
         break;
       case 'month':
-        this.selectedMonthIndex = index;
+        this.monthValue = getMonthIndex(value?.toString());
+        this.selectedMonthIndex = indexValue;
         break;
       case 'year':
-        this.selectedYearIndex = index;
-        break;
-      default:
+        this.selectedYearIndex = indexValue;
         break;
     }
 
-    const selectedDate = getDates()[this.selectedDateIndex];
-    const selectedYear = getYearRange()[this.selectedYearIndex];
-    const dateObj = getDateObject(
-      selectedDate,
-      this.selectedMonthIndex,
-      selectedYear,
+    const minYear = this.getMinValue('year');
+    const maxYear = this.getMaxValue('year');
+    const selectedYear = getYearRange(minYear, maxYear)[this.selectedYearIndex];
+
+    // Restrict months dynamically
+    let minMonth = 0;
+    let maxMonth = 11;
+    if (selectedYear === minYear) minMonth = this.getMinValue('month');
+    if (selectedYear === maxYear) maxMonth = this.getMaxValue('month');
+
+    // Update month data dynamically
+    this.monthData = getMonths(minMonth, maxMonth);
+
+    // Ensure selected month is within range
+    let minDate = 1;
+    let maxDate: number = 31;
+
+    if (this.monthValue > maxMonth) {
+      this.selectedMonthIndex = maxMonth;
+    }
+
+    maxDate = getDaysInMonth(
+      getMonthIndex(this.monthData[this.selectedMonthIndex]) + 1,
+      selectedYear
     );
 
-    this.props?.onDateChange?.(dateObj);
+    if (this.monthValue < minMonth) {
+      this.monthValue = minMonth;
+      maxDate = getDaysInMonth(this.monthValue + 1, selectedYear);
+
+    }
+
+    // Restrict days dynamically
+    const selectedMonth = this.selectedMonthIndex;
+    if (selectedYear === minYear && this.monthValue === minMonth) minDate = this.getMinValue('date');
+
+    if (selectedYear === maxYear && selectedMonth === maxMonth) maxDate = this.getMaxValue('date');
+
+
+    // Update date data dynamically
+    this.dateData = getDates(minDate, maxDate);
+
+    // Ensure selected date is within range
+    if (this.selectedDateIndex > maxDate - 1) this.selectedDateIndex = maxDate - 1;
+
+    const selectedDate = this.dateData[this.selectedDateIndex] || this.dateData[this.dateData.indexOf(this.selectedDateIndex + 1)];;
+
+
+    // Create new selected date
+    const dateObj = getDateObject(selectedDate, getMonthIndex(this.monthData[this.selectedMonthIndex]), selectedYear);
+    this.localSelectedDate = dateObj;
+
+    this.props?.onDateChange?.(this.localSelectedDate);
   };
 
   render() {
     return (
       <View style={styles.root}>
         <WmWheelScrollPicker
-          data={getDates()}
+          data={this.dateData}
           wrapperHeight={this.wrapperHeight}
           itemHeight={this.itemHeight}
           selectedIndex={this.selectedDateIndex}
-          onValueChange={(value: string | number, index: number) =>
+          onValueChange={(value, index) =>
             this.handleValueChange('date', value, index)
           }
         />
         <View style={styles.middle}>
           <WmWheelScrollPicker
-            data={getMonths()}
+            data={this.monthData}
             wrapperHeight={this.wrapperHeight}
             itemHeight={this.itemHeight}
             selectedIndex={this.selectedMonthIndex}
-            onValueChange={(value: string | number, index: number) =>
+            onValueChange={(value, index) =>
               this.handleValueChange('month', value, index)
             }
           />
         </View>
         <WmWheelScrollPicker
-          data={getYearRange()}
+          data={getYearRange(this.getMinValue('year'), this.getMaxValue('year'))}
           wrapperHeight={this.wrapperHeight}
           itemHeight={this.itemHeight}
           selectedIndex={this.selectedYearIndex}
-          onValueChange={(value: string | number, index: number) =>
+          onValueChange={(value, index) =>
             this.handleValueChange('year', value, index)
           }
         />
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 import moment from "moment";
 import * as FileSystem from "expo-file-system";
-import { isFunction, includes, isUndefined, isNull, orderBy, groupBy, toLower, get, forEach, sortBy, cloneDeep, keys, values, isArray, isString, isNumber} from 'lodash';
+import { isFunction, includes, isUndefined, isNull, orderBy, groupBy, toLower, get, forEach, sortBy, cloneDeep, keys, values, isArray, isString, isNumber } from 'lodash';
 import ThemeVariables from '../styles/theme.variables';
 
 
@@ -20,25 +20,25 @@ const TIME_ROLLUP_OPTIONS = {
 };
 
 const _deepCopy = (o1: any, ...o2: any) => {
-    o2.forEach((o: any) => {
-        if (o) {
-            Object.keys(o).forEach(k => {
-                const v = o[k];
-                if (v && !isString(v) && !isArray(v) && typeof v === 'object') {
-                    o1[k] = _deepCopy(o1[k] || {}, o[k]);
-                } else {
-                    o1[k] = _deepCopy(v);
-                }
-            });
+  o2.forEach((o: any) => {
+    if (o) {
+      Object.keys(o).forEach(k => {
+        const v = o[k];
+        if (v && !isString(v) && !isArray(v) && typeof v === 'object') {
+          o1[k] = _deepCopy(o1[k] || {}, o[k]);
+        } else {
+          o1[k] = _deepCopy(v);
         }
-    });
-    return o1;
+      });
+    }
+  });
+  return o1;
 };
 
 export const deepCopy = (...objects: any) => _deepCopy({}, ...objects);
 
 export const toBoolean = (val: any) => {
-  return  val === true
+  return val === true
     || val === 'true'
     || !(val === false
       || val === null
@@ -49,7 +49,7 @@ export const toBoolean = (val: any) => {
 export const toNumber = (val: any) => {
   try {
     return parseFloat(val) || 0;
-  } catch(e) {
+  } catch (e) {
     return 0;
   }
 };
@@ -57,18 +57,18 @@ export const toNumber = (val: any) => {
 /**
  * this method encodes the url and returns the encoded string
  */
- export const encodeUrl = (url: string): string => {
-    let splits = url.split('#');
-    const hash = splits[1];
-    splits = splits[0].split('?');
-    let params = '';
-    if (splits.length > 1) {
-        params = splits[1].split('&')
-            .map(p => p.split('='))
-            .map(p => p[0] +'=' + encodeURIComponent(p[1]))
-            .join('&');
-    }
-    return encodeURI(splits[0]) + (params ? '?' + params: '') + (hash ? '#'+ hash : '');
+export const encodeUrl = (url: string): string => {
+  let splits = url.split('#');
+  const hash = splits[1];
+  splits = splits[0].split('?');
+  let params = '';
+  if (splits.length > 1) {
+    params = splits[1].split('&')
+      .map(p => p.split('='))
+      .map(p => p[0] + '=' + encodeURIComponent(p[1]))
+      .join('&');
+  }
+  return encodeURI(splits[0]) + (params ? '?' + params : '') + (hash ? '#' + hash : '');
 };
 
 export const isWebPreviewMode = () => Platform.OS === 'web';
@@ -81,7 +81,7 @@ export const isAndroid = () => (Platform.OS === 'android' || (Platform.OS === 'w
 
 export const isIos = () => (Platform.OS === 'ios' || (Platform.OS === 'web' && /iPhone|iPad/i.test(window.navigator.userAgent)));
 
-const getGroupKey = (fieldDef: any, groupby: string,  widgetScope: any, innerItem?: any) => isFunction(groupby) ? groupby.apply(widgetScope.proxy, [innerItem ? fieldDef[innerItem] : fieldDef]) : get(innerItem ? fieldDef[innerItem] : fieldDef, groupby);
+const getGroupKey = (fieldDef: any, groupby: string, widgetScope: any, innerItem?: any) => isFunction(groupby) ? groupby.apply(widgetScope.proxy, [innerItem ? fieldDef[innerItem] : fieldDef]) : get(innerItem ? fieldDef[innerItem] : fieldDef, groupby);
 
 /**
  * This method prepares the grouped data.
@@ -141,7 +141,7 @@ export const getOrderedDataset = (dataSet: any, orderby: string, innerItem?: any
   const items = orderby && orderby.split(','),
     fields: any = [],
     directions: any = [];
-  items && items.forEach( obj => {
+  items && items.forEach(obj => {
     const item = obj.split(':');
     fields.push(innerItem ? innerItem + '.' + item[0] : item[0]);
     directions.push(item[1]);
@@ -212,27 +212,27 @@ export const unStringify = (val: any, defaultVal?: boolean) => {
 export const retryIfFails = (fn: Function, interval: number, maxRetries: number, onBeforeRetry = () => Promise.resolve(false)) => {
   let retryCount = 0;
   const tryFn = () => {
-      retryCount++;
-      if (isFunction(fn)) {
-          return fn();
-      }
+    retryCount++;
+    if (isFunction(fn)) {
+      return fn();
+    }
   };
   maxRetries = (isNumber(maxRetries) && maxRetries > 0 ? maxRetries : 0);
   interval = (isNumber(interval) && interval > 0 ? interval : 0);
   return new Promise((resolve, reject) => {
-      const errorFn = function () {
-          const errArgs = arguments;
-          setTimeout(() => {
-              Promise.resolve().then(() => onBeforeRetry()).then(function (retry) {
-                  if (retry !== false && (!maxRetries || retryCount <= maxRetries)) {
-                    Promise.resolve().then(() => tryFn()).then(resolve, errorFn);
-                  } else {
-                      reject(errArgs);
-                  }
-              }, () => reject(errArgs));
-          }, interval);
-      };
-      Promise.resolve().then(() => tryFn()).then(resolve, errorFn);
+    const errorFn = function () {
+      const errArgs = arguments;
+      setTimeout(() => {
+        Promise.resolve().then(() => onBeforeRetry()).then(function (retry) {
+          if (retry !== false && (!maxRetries || retryCount <= maxRetries)) {
+            Promise.resolve().then(() => tryFn()).then(resolve, errorFn);
+          } else {
+            reject(errArgs);
+          }
+        }, () => reject(errArgs));
+      }, interval);
+    };
+    Promise.resolve().then(() => tryFn()).then(resolve, errorFn);
   });
 };
 
@@ -242,20 +242,20 @@ export const retryIfFails = (fn: Function, interval: number, maxRetries: number,
  */
 export const getAbortableDefer = () => {
   const _defer: any = {
-      promise: null,
-      reject: null,
-      resolve: null,
-      onAbort: () => { },
-      isAborted: false
+    promise: null,
+    reject: null,
+    resolve: null,
+    onAbort: () => { },
+    isAborted: false
   };
   _defer.promise = new Promise((resolve, reject) => {
-      _defer.resolve = resolve;
-      _defer.reject = reject;
+    _defer.resolve = resolve;
+    _defer.reject = reject;
   });
   _defer.promise.abort = () => {
-      _defer.onAbort && _defer.onAbort();
-      _defer.reject('aborted');
-      _defer.isAborted = true;
+    _defer.onAbort && _defer.onAbort();
+    _defer.reject('aborted');
+    _defer.isAborted = true;
   };
   return _defer;
 };
@@ -270,7 +270,7 @@ export const validateField = (props: any, value: any) => {
     }
     if (!requiredCheck) {
       return {
-        errorType : 'required',
+        errorType: 'required',
         isValid: false
       }
     }
@@ -280,27 +280,27 @@ export const validateField = (props: any, value: any) => {
     regexCheck = condition.test(value);
     if (!regexCheck) {
       return {
-        errorType : 'regexp',
+        errorType: 'regexp',
         isValid: false
       }
     }
   }
   if (value && props.maxchars && value.length > props.maxchars) {
     return {
-      errorType : 'maxchars',
+      errorType: 'maxchars',
       isValid: false
     }
 
   }
   if (value && props.mindate && new Date(props.datavalue) < moment(props.mindate).startOf('day').toDate()) {
     return {
-      errorType : 'mindate',
+      errorType: 'mindate',
       isValid: false
     }
   }
   if (value && props.maxdate && new Date(props.datavalue) > moment(props.maxdate).endOf('day').toDate()) {
     return {
-      errorType : 'maxdate',
+      errorType: 'maxdate',
       isValid: false
     }
   }
@@ -315,7 +315,7 @@ export const countDecimalDigits = (number: number | string) => {
 
   // * convert the number to a string
   const numberString = number.toString();
-  
+
   // * regular expression to match and count the decimal digits
   const decimalMatch = numberString.match(/\.(\d+)/);
 
@@ -332,31 +332,31 @@ export const formatCompactNumber = (number: number) => {
   number = isNegative ? number * -1 : number;
   let formattedNumber = number + '';
   if (number >= 1000 && number < 1_000_000) {
-    formattedNumber =  (number / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    formattedNumber = (number / 1000).toFixed(1).replace(/\.0$/, "") + "K";
   } else if (number >= 1_000_000 && number < 1_000_000_000) {
-    formattedNumber =  (number / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
+    formattedNumber = (number / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
   } else if (number >= 1_000_000_000 && number < 1_000_000_000_000) {
-    formattedNumber =  (number / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B";
+    formattedNumber = (number / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B";
   } else if (number >= 1_000_000_000_000 && number < 1_000_000_000_000_000) {
-    formattedNumber =  (number / 1_000_000_000_000).toFixed(1).replace(/\.0$/, "") + "T";
+    formattedNumber = (number / 1_000_000_000_000).toFixed(1).replace(/\.0$/, "") + "T";
   }
   return (isNegative ? '-' : '') + formattedNumber;
 }
 
-export const toBase64 = function(path: string) {
+export const toBase64 = function (path: string) {
   return FileSystem.readAsStringAsync(path, { encoding: 'base64' });
 };
 
-const DATASET_WIDGETS = new Set([ 'select', 'checkboxset', 'radioset', 'switch', 'autocomplete', 'chips', 'typeahead', 'rating']);
+const DATASET_WIDGETS = new Set(['select', 'checkboxset', 'radioset', 'switch', 'autocomplete', 'chips', 'typeahead', 'rating']);
 export const isDataSetWidget = (widget: any) => {
   return DATASET_WIDGETS.has(widget);
 };
 export const isFullPathUrl = (url: string) => {
   return isString(url) &&
-  (url.startsWith('data:') 
-  || url.startsWith('http:') 
-  || url.startsWith('https:') 
-  || url.startsWith('file:'));
+    (url.startsWith('data:')
+      || url.startsWith('http:')
+      || url.startsWith('https:')
+      || url.startsWith('file:'));
 };
 
 export function removeUndefinedKeys(obj: any) {
@@ -372,19 +372,24 @@ export function removeUndefinedKeys(obj: any) {
   return obj;
 }
 // * get total number of days in a month of a year
-function getDaysInMonth(month: number, year: number) {
+export function getDaysInMonth(month: number, year: number) {
   return new Date(year, month, 0).getDate();
 }
 
 export const getDates = (
+  startDate: number,
+  endDate: number,
   month = 0, // zero-based
   year = new Date().getFullYear(),
 ) => {
   const daysInMonth = getDaysInMonth(month, year);
-  const dates = Array.from({length: daysInMonth}, (v, i) => i + 1);
+  const dates = Array.from({ length: daysInMonth }, (v, i) => i + 1);
 
-  return dates;
+  const datesInRange = dates.filter(date => date >= startDate && date <= endDate);
+
+  return datesInRange;
 };
+
 
 export const monthNames = [
   'January',
@@ -401,11 +406,14 @@ export const monthNames = [
   'December',
 ];
 
-export const getMonths = () => {
+export const getMonths = (startMonth: number, endMonth: number) => {
   const months = monthNames.map(name => name.substring(0, 3));
+  const monthRange = months.filter((_, index) => index >= startMonth && index <= endMonth);
 
-  return months;
+  return monthRange;
 };
+
+
 
 export const getYearRange = (
   startYear: number = 1950,
@@ -484,7 +492,7 @@ export const getGradientStartEnd = (angle: string) => {
     // other angle
   }
 
-  return {start, end}
+  return { start, end }
 }
 
 export const parseLinearGradient = (gradient: string) => {
@@ -494,18 +502,18 @@ export const parseLinearGradient = (gradient: string) => {
 
   const matches = gradient?.match(linearGradientRegex);
   angle = matches?.[1] || '90deg';
-  const {start, end} = getGradientStartEnd(angle)
+  const { start, end } = getGradientStartEnd(angle)
   color1 = matches?.[2] || ThemeVariables.INSTANCE.primaryColor;
   color2 = matches?.[3] || ThemeVariables.INSTANCE.primaryColor;
 
-  return {hasLinearGradient, color1, color2, start, end};
+  return { hasLinearGradient, color1, color2, start, end };
 }
 
 export const validateInputOnDevice = (value: string, type: 'number' | 'currency') => {
   const isCurrencyField = type === 'currency';
   let isValidText = true;
   let validText = value;
-  
+
   // * no alphabets except E, may contain E only once
   if (/[a-df-zA-DF-Z]/.test(value) || !/^[^eE]*[eE]?[^eE]*$/.test(value)) {
     isValidText = false;
@@ -538,7 +546,7 @@ export const validateInputOnDevice = (value: string, type: 'number' | 'currency'
     validText = validText.replace(/[\s,]/, '');
   }
 
-  return {isValidText, validText};
+  return { isValidText, validText };
 }
 
 export const isDateFormatAsPerPattern = (
@@ -556,3 +564,11 @@ export const isDateFormatAsPerPattern = (
     return false;
   }
 };
+
+export const getMonthIndex = (monthName: string, shortName: boolean = true) => {
+  const months = shortName ? monthNames.map(name => name.substring(0, 3)) : monthNames;
+  const monthNumber = months.findIndex((name) => name === monthName);
+
+  return monthNumber;
+}
+
