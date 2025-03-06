854a057af8f4178d07861280ff033096343ede11
Swathi Anupuram <swathi.anupuram@wavemaker.com>
2025-03-06 09:13:56 +0000

    WMS-27522 bug regarding default formdata not showing in form/live form fixed. added support for array as default formdata with its unit test.
    (cherry picked from commit 24b875ede32331c5bb978e338652eaa1743f2c77)
    
    Co-authored-by: mayankPrakashWavemaker <mayank.prakash@wavemaker.com>

--- src/components/data/form/form.component.tsx
+++ src/components/data/form/form.component.tsx
@@ -49,7 +49,7 @@ export default class WmForm extends BaseComponent<WmFormProps, WmFormState, WmFo
   getParentFormRef(pformName: string) {
     let current = this.parent;
     while (current) {
-      if (get(current, 'props.name') === pformName) {
+      if (pformName && (get(current, 'props.name') === pformName)) {
         this.parentFormRef = current;
         break;
       }
@@ -170,6 +170,9 @@ export default class WmForm extends BaseComponent<WmFormProps, WmFormState, WmFo
       }
       let key = get(formField, 'formKey') || get(fw, 'props.name');
       fw && fw.setState({ isDefault: true });
+      if(Array.isArray(formData)){
+        formData = formData[0];
+      }
       formField.updateState({
         props: {
           datavalue: get(formData, key)
