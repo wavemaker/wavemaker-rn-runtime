d3b795f8b3bab4ef903bb6f4bdc1fef5369efca2
Nishanth <nishanth.vishwanatha@wavemaker.com>
2025-03-18 14:32:27 +0530

    WMS-27803 | Feat: Implemented swipe up/down events for page-content components

--- src/components/page/page-content/page-content.props.ts
+++ src/components/page/page-content/page-content.props.ts
@@ -4,4 +4,6 @@ import { SkeletonAnimationProps } from '@wavemaker/app-rn-runtime/runtime/base-f
 export default class WmPageContentProps extends SkeletonAnimationProps {
     children: any[] = null as any;
     scrollable: boolean = true;
+    onSwipeup?: Function = null as any;  
+    onSwipedown?: Function = null as any;
 }
