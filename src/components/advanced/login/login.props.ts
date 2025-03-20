afa645a8eac1105e5e7c198f3103230a88bdfa2c
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2025-03-20 15:41:53 +0530

    WMS-27828 - added prop to enable/disable default err msg in login component

--- src/components/advanced/login/login.props.ts
+++ src/components/advanced/login/login.props.ts
@@ -3,4 +3,5 @@ import { BaseProps } from '@wavemaker/app-rn-runtime/core/base.component';
 export default class WmLoginProps extends BaseProps {
     children: any;
     onLogin: Function = () => {};
+    showerror: boolean = true;
 }
