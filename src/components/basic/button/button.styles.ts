c618203b1fde7cfb8ed98190a2158ab7aee1f031
Aamer Sohail <aamersohail@Aamers-MacBook-Pro-10.local>
2025-04-17 17:07:23 +0530

    WMS-26859 Alignself-button-prop-removed

--- src/components/basic/button/button.styles.ts
+++ src/components/basic/button/button.styles.ts
@@ -20,7 +20,6 @@ BASE_THEME.registerStyle((themeVariables, addStyle) => {
           paddingBottom: 12,
           paddingLeft: 24,
           paddingRight: 24,
-          alignSelf: 'flex-start',
           rippleColor: themeVariables.rippleColor
       },
       content: {
