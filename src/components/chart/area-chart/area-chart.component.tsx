3ef6cc9a0ad7f9e069382d9c510adfd14a46ad63
Virri Praneeth Reddy <praneeth.reddy@wavemaker.com>
2025-04-30 15:10:30 +0530

    WMS-28195: these changes make sure that there won't be any empty space in the positions of title and subheading when no value is given for them in studio

--- src/components/chart/area-chart/area-chart.component.tsx
+++ src/components/chart/area-chart/area-chart.component.tsx
@@ -82,11 +82,14 @@ export default class WmAreaChart extends BaseChartComponent<WmAreaChartProps, Wm
         key={`${props.title}_area_chart`}
       >
         <View>
+          { props.title? (
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
             {props.iconclass ? (<WmIcon iconclass={props.iconclass} styles={this.styles.icon}></WmIcon>) : null }
             <Text style={this.styles.title}>{props.title}</Text>
           </View>
-          <Text style={this.styles.subHeading}>{props.subheading}</Text>
+          ) : null }
+          { props.subheading? (
+          <Text style={this.styles.subHeading}>{props.subheading}</Text> ) : null }
         </View>
         {this.state.chartWidth ? 
         (
