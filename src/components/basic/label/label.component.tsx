f4567ac5e01edd0a2a206b89a2e191d67f53d6ba
Virri Praneeth Reddy <praneeth.reddy@wavemaker.com>
2025-05-05 17:30:20 +0530

    WMS-28214: changes to make ellipsis work for android

--- src/components/basic/label/label.component.tsx
+++ src/components/basic/label/label.component.tsx
@@ -123,6 +123,21 @@ export default class WmLabel extends BaseComponent<WmLabelProps, WmLabelState, W
     }
   }
 
+  private shouldUseAndroidEllipsis(props: WmLabelProps): boolean {
+    return Platform.OS === 'android' && props.ellipsisenabledforandroid !== false;
+  }
+
+  // Helper to get the proper number of lines
+  private getNumberOfLines(props: WmLabelProps): number | undefined {
+      let numOfLines: number | undefined = undefined;
+      if (props.wrap === false) {
+          numOfLines = 1;
+      } else if (typeof props.nooflines === 'number' && props.nooflines > 0) {
+          numOfLines = props.nooflines;
+      }
+      return numOfLines;
+  }
+
   private renderLabelTextContent(navigationService: NavigationService, isHidden: boolean = false, hasLinearGradient: boolean = false) {
     //gradient text support for web
     const gradientTextWebStyle = {
@@ -137,8 +152,38 @@ export default class WmLabel extends BaseComponent<WmLabelProps, WmLabelState, W
     const hiddenStyle = isHidden ? { opacity: 0 } : {};
     const gradientStyle = showWebTextGradient ? gradientTextWebStyle : {};
 
+    const numOfLines = this.getNumberOfLines(this.state.props);
+    const useAndroidEllipsis = this.shouldUseAndroidEllipsis(this.state.props);
+
     // Determine if it's a single part
     const isSinglePart = this.state.parts.length <= 1;
+
+    if (useAndroidEllipsis) {
+      const androidEllipsisTextStyle: any = {
+          ...baseStyle,
+          ...hiddenStyle,
+          ...gradientStyle,
+          userSelect: undefined,
+          // Do NOT apply gradientStyle here when using Android ellipsis
+          width: '100%',
+          textAlign: baseStyle.textAlign,
+      };
+      return (
+          <Text
+              style={androidEllipsisTextStyle}
+              numberOfLines={numOfLines}
+              ellipsizeMode="tail"
+              {...(this.state.parts.length <= 1 ? this.getTestPropsForLabel('caption') : {})}
+              {...getAccessibilityProps(AccessibilityWidgetType.LABEL, this.state.props)}
+          >
+              {this.state.parts.length === 1
+                  ? toString(this.state.props.caption)
+                  : this.state.parts.map(part => toString(part.text)).join('')}
+              {this.state.props.required && this.getAsterisk()}
+          </Text>
+      );
+    }  
+
 
     // Compose final style
     const combinedTextStyle = isSinglePart
