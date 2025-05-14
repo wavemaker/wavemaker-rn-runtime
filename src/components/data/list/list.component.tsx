28603afdb40b8b6162e777c938c8f198be3175f5
Nishanth <nishanth.vishwanatha@wavemaker.com>
2025-05-14 18:50:01 +0530

    WMS-28265 | Fix: Duplicate API Calls and Missing Pages

--- src/components/data/list/list.component.tsx
+++ src/components/data/list/list.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ActivityIndicator, SectionList, Text, View, FlatList, LayoutChangeEvent, TouchableOpacity } from 'react-native';
 import { isArray, isEmpty, isNil, isNumber, round } from 'lodash-es';
+import {debounce} from 'lodash'
 import { BaseComponent, BaseComponentState } from '@wavemaker/app-rn-runtime/core/base.component';
 import { getGroupedData, getNumberOfEmptyObjects, isDefined } from "@wavemaker/app-rn-runtime/core/utils";
 import { Tappable } from '@wavemaker/app-rn-runtime/core/tappable.component';
@@ -128,7 +129,7 @@ export default class WmList extends BaseComponent<WmListProps, WmListState, WmLi
     ) : null;
   };
 
-  private loadData() {
+  private loadData = debounce(() => {
     if (this.state.loadingData || !this.hasMoreData) {
       return;
     }
@@ -174,7 +175,7 @@ export default class WmList extends BaseComponent<WmListProps, WmListState, WmLi
         }, 1000);
       });
     }
-  }
+  },300)
 
   private selectFirstItem() {
     const props = this.state.props;
