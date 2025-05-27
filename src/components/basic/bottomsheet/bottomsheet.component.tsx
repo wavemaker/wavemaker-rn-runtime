d5616408e280fce99aa47ac878e1225165a68826
Nishanth <nishanth.vishwanatha@wavemaker.com>
2025-05-27 19:55:44 +0530

    WMS-28234 | update:swipe gesture added

--- src/components/basic/bottomsheet/bottomsheet.component.tsx
+++ src/components/basic/bottomsheet/bottomsheet.component.tsx
@@ -1,6 +1,6 @@
 import React, { createRef } from 'react';
 import { BaseComponent, BaseComponentState } from '@wavemaker/app-rn-runtime/core/base.component';
-import { Text, View, Animated, PanResponder, Dimensions, TouchableWithoutFeedback, Platform, ScrollView,SafeAreaView, StatusBar,BackHandler } from 'react-native';
+import { Text, View, Animated, PanResponder, Dimensions, TouchableWithoutFeedback, Platform, ScrollView, PanResponderGestureState, StatusBar, BackHandler } from 'react-native';
 import WmBottomsheetProps from './bottomsheet.props';
 import { DEFAULT_CLASS, WmBottomsheetStyles } from './bottomsheet.styles';
 
@@ -25,38 +25,37 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
   private expandedDefaultHeight: number = 0.5;
   private maxHeight: number = 1.0; // Allow full screen height
   private isBottomsheetVisible: boolean = false;
-  private animationDuration:number = 400
-  private statusBarHeight:number = StatusBar.currentHeight || 0;
-  private defaultTopInset:number = 44;
+  private animationDuration: number = 400
+  private statusBarHeight: number = StatusBar.currentHeight || 0;
+  private defaultTopInset: number = 44;
+  private maxHeightRatio: number = 0;
 
   private calculateSheetHeight(sheetheightratio: number): number {
     // Allow full range from 0.2 to 1.0 (100% of screen height)
-    const maxHeightRatio = Math.min(
-      sheetheightratio >= this.defaultHeight ? sheetheightratio : this.defaultHeight, 
+    this.maxHeightRatio = Math.min(
+      sheetheightratio >= this.defaultHeight ? sheetheightratio : this.defaultHeight,
       this.maxHeight
     );
-    
-    let calculatedHeight = SCREEN_HEIGHT * maxHeightRatio;
-    
-    // Add safe area top inset for iOS
+
+    let calculatedHeight = SCREEN_HEIGHT * this.maxHeightRatio;
+
     if (Platform.OS === 'ios') {
-       // Common value for iPhone X and newer
-      // Subtract top inset  bar height for ios only if sheet height ration is 0.9
-      if (maxHeightRatio >= 0.9) {
+      // Subtract top inset bar height for ios only if sheetheightratio is 0.9
+      if (this.maxHeightRatio >= 0.9) {
         calculatedHeight -= this.defaultTopInset;
       }
-    } 
-    else if (Platform.OS === 'android') {
-      // Subtract status bar height for Android only if sheet height ration is 0.9
-      if (maxHeightRatio >= 0.9) {
-         calculatedHeight -= this.statusBarHeight;
-      }
-     
     }
-    
+    else if (Platform.OS === 'android') {
+      // Subtract status bar height for Android only if sheetheightratio is 0.9
+      if (this.maxHeightRatio >= 0.9) {
+        calculatedHeight -= this.statusBarHeight;
+      }
+
+    }
+
     console.log("device height is", SCREEN_HEIGHT);
-    console.log(maxHeightRatio,"height is", SCREEN_HEIGHT*maxHeightRatio);
-    console.log("height ratio is", maxHeightRatio);
+    console.log(this.maxHeightRatio, "height is", SCREEN_HEIGHT * this.maxHeightRatio);
+    console.log("height ratio is", this.maxHeightRatio);
     console.log("calculated height is", calculatedHeight);
     return calculatedHeight;
   }
@@ -64,29 +63,29 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
   constructor(props: WmBottomsheetProps) {
     super(props, DEFAULT_CLASS, new WmBottomsheetProps(), new WmBottomsheetState());
     this.calculatedHeight = this.calculateSheetHeight(props.sheetheightratio);
-    
+
     // Allow expanded height to be full screen
     const expandedRatio = props.expandedheightratio || this.expandedDefaultHeight;
 
-  
+
     this.expandedHeight = SCREEN_HEIGHT * Math.max(
-      this.expandedDefaultHeight, 
+      this.expandedDefaultHeight,
       Math.min(expandedRatio, this.maxHeight)
     );
 
-    if(Platform.OS === 'android') {
-      // Subtract status bar height for Android only if sheet height ration is 0.9
+    if (Platform.OS === 'android') {
+      // Subtract top inset bar height for ios only if sheetheightratio is 0.9
       if (expandedRatio >= 0.9) {
-         this.expandedHeight -= this.statusBarHeight;
+        this.expandedHeight -= this.statusBarHeight;
       }
-     
+
     }
     if (Platform.OS === 'ios') {
-      // Subtract top inset  bar height for ios only if sheet height ration is 0.9
+      // Subtract status bar height for Android only if sheetheightratio is 0.9
       if (expandedRatio >= 0.9) {
         this.expandedHeight -= this.defaultTopInset;
       }
-    } 
+    }
     this.state.sheetHeight.setValue(this.calculatedHeight);
 
     this.isBottomsheetVisible = this.props.visible || false;
@@ -126,6 +125,47 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
     }
   }
 
+  handleSwipeGesture = (gestureState: PanResponderGestureState) => {
+    this.updateState({
+      lastGestureDy: 0
+    } as WmBottomsheetState);
+    if (gestureState.dy > 0) {
+      if (this.state.isExpanded) {
+        console.log("user removed finger in ", gestureState.dy)
+        console.log("bottom sheet threshold ", this.expandedHeight / 2)
+        if (gestureState.dy < this.expandedHeight / 2) {
+          console.log("user swiped less than bottom sheet threshold")
+          Animated.parallel([
+            Animated.timing(this.state.translateY, {
+              toValue: 0, // Keep sheet open
+              duration: this.animationDuration,
+              useNativeDriver: false,
+            }),
+            Animated.timing(this.state.sheetHeight, {
+              toValue: this.calculatedHeight, // Back to original height
+              duration: this.animationDuration,
+              useNativeDriver: false,
+            })
+          ]).start();
+          this.updateState({
+            isExpanded: false
+          } as WmBottomsheetState);
+        }
+        else if (gestureState.dy > this.expandedHeight / 2 || gestureState.vy > 0.5) {
+          console.log("user swiped more bottom sheet threshold")
+          this.closeSheet();
+        }
+      }
+      else {
+        if (gestureState.dy > 100 || gestureState.vy > 0.5) {
+          this.closeSheet();
+        } else {
+          this.openSheet();
+        }
+      }
+    }
+  }
+  // panResponder for bottom sheet scroll view
   panResponder = PanResponder.create({
     onStartShouldSetPanResponder: (_, gestureState) => {
       // Only handle the gesture if we're at the top and swiping down
@@ -141,19 +181,18 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
       if (gestureState.dy > 0) {
         const newTranslateY = Math.max(0, this.state.lastGestureDy + gestureState.dy);
         this.state.translateY.setValue(newTranslateY);
+        if (this.state.isExpanded) {
+          console.log("Y value ", gestureState.dy)
+        }
       }
     },
 
     onPanResponderRelease: (_, gestureState) => {
-      this.updateState({
-        lastGestureDy: 0
-      } as WmBottomsheetState);
+      this.handleSwipeGesture(gestureState)
 
-      if (gestureState.dy > 100 || gestureState.vy > 0.5) {
-        this.closeSheet();
-      } else {
-        this.openSheet();
-      }
+
+
+
     },
 
     onPanResponderTerminate: () => {
@@ -161,15 +200,16 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
     },
   });
 
+  //pan repsoneder for bottom sheet dragable container
   dragHandlePanResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onMoveShouldSetPanResponder: () => true,
     onPanResponderMove: (_, gestureState) => {
-      console.log("y values ",gestureState.dy)
+
       if (gestureState.dy > 0) { // Handle downward drag
         const newTranslateY = Math.max(0, this.state.lastGestureDy + gestureState.dy);
         this.state.translateY.setValue(newTranslateY);
-      } else if (gestureState.dy < 0 && this.props.bottompopup && this.props.sheetheightratio !==1) { 
+      } else if (gestureState.dy < 0 && this.props.bottompopup && this.props.sheetheightratio !== 1) {
         // Handle upward drag - expand to full height
         // Allow expansion to full screen height
         const targetHeight = Math.min(this.expandedHeight, SCREEN_HEIGHT);
@@ -184,11 +224,7 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
       }
     },
     onPanResponderRelease: (_, gestureState) => {
-      if (gestureState.dy > 100 || gestureState.vy > 0.5) {
-        this.closeSheet();
-      } else {
-        this.openSheet();
-      }
+      this.handleSwipeGesture(gestureState)
     },
   });
 
@@ -204,7 +240,7 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
     this.updateState({
       lastGestureDy: 0,
     } as WmBottomsheetState);
-    
+
     Animated.parallel([
       Animated.timing(this.state.translateY, {
         toValue: 0,
@@ -278,15 +314,15 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
 
   renderWidget(props: WmBottomsheetProps) {
     if (!this.isBottomsheetVisible || !props.visible) return null;
-    
+
     return (
-     
-       <View style={this.styles.root}>
-         {this._background}
+
+      <View style={this.styles.root}>
+        {this._background}
         <TouchableWithoutFeedback onPress={this.closeSheet}>
           <Animated.View style={[this.styles.backdrop, { opacity: this.state.backdropOpacity }]} />
         </TouchableWithoutFeedback>
-        
+
         <Animated.View
           style={[
             this.styles.sheetContainer,
@@ -320,7 +356,7 @@ export default class WmBottomsheet extends BaseComponent<WmBottomsheetProps, WmB
           </ScrollView>
         </Animated.View>
       </View>
-     
+
     );
   }
 }
