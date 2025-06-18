cb99726a8ef89a07f7b34a13a84f5f80a659e283
premk_500362 <premkumar.katta@wavemaker.com>
2025-06-19 02:35:22 +0530

    WMS-27412 | sticky-header | Fix: stickyHeader translateY position after pageContent scrollview settled

--- src/components/container/container.component.tsx
+++ src/components/container/container.component.tsx
@@ -10,6 +10,7 @@ import { WmSkeletonStyles } from '../basic/skeleton/skeleton.styles';
 import { ScrollView } from 'react-native-gesture-handler';
 import { StickyContextType, StickyContext, StickyHeader } from '@wavemaker/app-rn-runtime/core/sticky-container.component';
 import { EdgeInsets, SafeAreaInsetsContext } from 'react-native-safe-area-context';
+import EventNotifier from '@wavemaker/app-rn-runtime/core/event-notifier';
 
 export class WmContainerState extends PartialHostState<WmContainerProps> {
   isPartialLoaded = false;
@@ -28,6 +29,14 @@ export default class WmContainer extends PartialHost<WmContainerProps, WmContain
     super(props, DEFAULT_CLASS, new WmContainerProps(), new WmContainerState());
     this.containerRef = React.createRef();
     this.stickyContainerOpacity = new Animated.Value(1);
+
+    EventNotifier.ROOT.subscribe('updateStickyHeaders', (_event: any) => {
+      if(this.props.issticky){
+        setTimeout(()=>{
+          this.getStickyHeaderTranslateY();
+        }, 500);
+      }
+    })
   }
 
   getBackground(): React.JSX.Element | null {
@@ -53,21 +62,18 @@ export default class WmContainer extends PartialHost<WmContainerProps, WmContain
       return null;
   }
 
-  private getStickyHeaderTranslateY(){
-    const { stickyHeaderTranslateY, pageContentReady } = this.context as StickyContextType;
+  public getStickyHeaderTranslateY(){
+    const { stickyHeaderTranslateY } = this.context as StickyContextType;
     this.containerRef?.current?.measure((_x = 0, _y = 0, _width = 0, _height = 0, px = 0, py = 0)=>{
       const topInsetsInYposition = Platform.OS == 'ios' ? (this.insets?.top || 0): 0
-      if(stickyHeaderTranslateY && pageContentReady) {
+      if(stickyHeaderTranslateY) {
         stickyHeaderTranslateY.value = py - topInsetsInYposition ;
-          this.updateState({ stickyContainerVisibility: true} as WmContainerState)
+        this.updateState({ stickyContainerVisibility: true} as WmContainerState);     
       }
     })
   }
 
   componentDidUpdate(_prevProps: any, prevState: any) {
-    const { pageContentReady } = this.context as StickyContextType;
-
-    if (pageContentReady) this.getStickyHeaderTranslateY();
     if (prevState.stickyContainerVisibility !== this.state.stickyContainerVisibility) {
       Animated.timing(this.stickyContainerOpacity, {
         toValue: this.state.stickyContainerVisibility ? 0 : 1,
@@ -81,7 +87,7 @@ export default class WmContainer extends PartialHost<WmContainerProps, WmContain
     const { stickyContainerVisibility } = this.state;
     return (
       <>
-        {stickyContainerVisibility && (
+        {stickyContainerVisibility ? (
           <StickyHeader
             component={this}
             theme={this.theme}
@@ -91,7 +97,7 @@ export default class WmContainer extends PartialHost<WmContainerProps, WmContain
               {this.renderContent(props)}
             </View>
           </StickyHeader>
-        )}
+        ) : <></>}
         <Animated.View 
           style={[
             dimensions as ViewStyle, 
@@ -130,7 +136,6 @@ export default class WmContainer extends PartialHost<WmContainerProps, WmContain
               delay={props.animationdelay} 
               style={styles}
               onLayout={(event: LayoutChangeEvent, ref: React.RefObject<View>) => {
-                if (props.issticky) this.getStickyHeaderTranslateY();
                 this.handleLayout(event, ref);
               }}
             >
