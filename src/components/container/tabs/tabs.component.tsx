fe97c30f2ddd0bac8dd185bb4bf5f6df6e8f2df4
Virri Praneeth Reddy <praneeth.reddy@wavemaker.com>
2025-06-24 16:59:55 +0530

    "WMS-28518: revert corrected the enablegestures prop where previously i hard-coded true but now it will take from props itself"
    This reverts commit 482d826fe68f4482def7899d44e920a9a9bbdfd6.

--- src/components/container/tabs/tabs.component.tsx
+++ src/components/container/tabs/tabs.component.tsx
@@ -248,7 +248,7 @@ export default class WmTabs extends BaseComponent<WmTabsProps, WmTabsState, WmTa
             maxHeight: this.tabPaneHeights[this.state.selectedTabIndex],
           }, this.styles.tabContent]} >
           <SwipeAnimation.View
-            enableGestures={props.enablegestures}
+            enableGestures={true}
             usenativegestures={props.usenativegestures}
             style={{
               flexDirection: 'row',
