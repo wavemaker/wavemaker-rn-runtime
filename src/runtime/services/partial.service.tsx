e5cc79619ba447385eae9f02a718a99bda4f8c78
Jyoti Das Ankur <jyotidas.ankur@wavemaker.com>
2025-07-14 10:43:39 +0000

    WMS-28630 | Fix: partials were not accessible inside popover when being used inside prefab

--- src/runtime/services/partial.service.tsx
+++ src/runtime/services/partial.service.tsx
@@ -8,10 +8,22 @@ export class PartialServiceImpl implements PartialService {
 
     }
 
-    get(partialName: string) {
+    get(partialName: string, params?: {isPrefab: boolean, name: string}) {
         if (!this.partials) {
             this.partials = injector.get<AppConfig>('APP_CONFIG').partials;
         }
+        
+        if(params?.isPrefab) {
+            let prefabPartials;
+            injector.get<AppConfig>('APP_CONFIG').prefabs?.partials?.forEach(item => {
+                if(item.prefabName === params.name){
+                    prefabPartials = item.partials;
+                }
+            })
+            const targetPartial = prefabPartials && (prefabPartials as any).find((p: any) => p.name === partialName);
+            return (targetPartial as any)?.component;
+        }
+
         const partial = this.partials.find((p: any) => p.name === partialName);
         return partial?.component;
     }
