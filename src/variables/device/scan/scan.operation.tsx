33d2684f8925563a9df5cc26525562546464b3ac
Vijay Tiwari <vijay.tiwari@wavemaker.com>
2025-08-20 12:21:37 +0530

    WIP: plugin provider and device service operation ~ WMS-25658

--- src/variables/device/scan/scan.operation.tsx
+++ src/variables/device/scan/scan.operation.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { ScanPluginConsumer, ScanPluginService, ScanService } from "@wavemaker/app-rn-runtime/core/device/scan-service";
+import { ScanPluginService, ScanService } from "@wavemaker/app-rn-runtime/core/device/scan-service";
 import { Operation, Input, Output } from '../operation.provider';
-import { PermissionConsumer, PermissionService } from "@wavemaker/app-rn-runtime/runtime/services/device/permission-service";
+import { PermissionService } from "@wavemaker/app-rn-runtime/runtime/services/device/permission-service";
 
 export interface ScanInput extends Input {
   barcodeFormat: string;
@@ -17,21 +16,9 @@ export interface ScanOutput extends Output {
 
 export class ScanOperation implements Operation {
 
-  constructor(private scan: ScanService) {}
+  constructor(private scan: ScanService, private permissionService: PermissionService, private scanPluginService: ScanPluginService) {}
 
   public invoke(params: ScanInput): any {
-    return (
-      <PermissionConsumer>
-        {(permissionService: PermissionService) => {
-          return (
-            <ScanPluginConsumer>
-              {(scanPluginService: ScanPluginService) => {
-                return this.scan.scanBarcode({ ...params, scanPluginService, permissionService });
-              }}
-            </ScanPluginConsumer>
-          );
-        }}
-      </PermissionConsumer>
-    );
+    return this.scan.scanBarcode({ ...params, scanPluginService: this.scanPluginService, permissionService: this.permissionService });
   }
 }
