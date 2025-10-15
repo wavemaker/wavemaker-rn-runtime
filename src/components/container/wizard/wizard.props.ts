88e8c647c49634e4315b05d14c68341a370404e0
hmonika23 <124656669+hmonika23@users.noreply.github.com>
2025-10-15 14:02:35 +0530

    WMS-29163|| Added iconclass prop for done button in wizard

--- src/components/container/wizard/wizard.props.ts
+++ src/components/container/wizard/wizard.props.ts
@@ -19,5 +19,6 @@ export default class WmWizardProps extends BaseProps {
   skipdefaultdone: boolean = false;
   skipdefaultskip: boolean = false; 
   skipdefaultcancel: boolean = false;
+  donebtniconclass: string = 'wm-sl-l sl-check';
 }
 
